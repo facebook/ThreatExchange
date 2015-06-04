e812791b42c16a809e9913d682562804f5335be6
Ivan Leichtling <ivanlei@yelp.com>
2015-06-04 07:33:08 -0700

    Add an `__all__` array to expose the public objects of the pytx module

--- pytx/pytx/__init__.py
+++ pytx/pytx/__init__.py
@@ -1,5 +1,6 @@
-# import vocabulary
-# from init import init
-# from malware import Malware
-# from threat_exchange_member import ThreatExchangeMember
-# from threat_indicator import ThreatIndicator
+from init import init
+from malware import Malware
+from threat_exchange_member import ThreatExchangeMember
+from threat_indicator import ThreatIndicator
+
+__all__ = ['init', 'Malware', 'ThreatExchangeMember', 'ThreatIndicator']
