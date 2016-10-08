1ceb0bfd034ef00fbde9e1b69e942f0fbfbf3372
Mike Goffin <mgoffin@mitre.org>
2016-10-08 14:24:12 -0400

    Adding docstrings and fixing custom content.
    Custom callback function and GET response now work. Added more
    documentation about what things are doing under the hood.

--- pytx/pytx/rtu.py
+++ pytx/pytx/rtu.py
@@ -6,7 +6,25 @@ class RTUListener(object):
 
     """
     This class will allow you to quickly instantiate a listener for Real-Time
-    Updates from ThreatExchange.
+    Updates from ThreatExchange. ThreatExchange will send a GET request to
+    confirm connectivity and will expect a string in the response which is
+    similar to the one you configure in your Webhook. This class also allows you
+    to specify a callback function. This function will receive the POST request
+    (push notification) from ThreatExchange allowing you to handle incoming data
+    as you see fit.
+
+    :param get_response: What to respond with when a GET request is made.
+    :type get_response: str
+    :param host: The IP to bind the listener to.
+    :type host: str
+    :param port: The port to bind the listener to.
+    :type port: int
+    :param listener_url: URL suffix to listen on (ex: /foo/)
+    :type listener_url: str
+    :param callback: Custom function you wish to use for POST requests.
+    :type callback: class
+    :param debug: Enable Flask debug mode (False by default)
+    :type debug: bool
     """
 
     def __init__(self,
@@ -25,15 +43,29 @@ class RTUListener(object):
         self.callback = callback
 
     def listen(self, host=None, port=None, debug=None):
+        """
+        Begin listening for incoming requests. Allows you to set a custom host,
+        port, and enable debug mode if you didn't already specify those when
+        instantiating the RTUListener object.
+
+        :type host: str
+        :param port: The port to bind the listener to.
+        :type port: int
+        :param debug: Enable Flask debug mode (False by default)
+        :type debug: bool
+        """
+
         host = host or self.host
         port = port or self.port
         debug = debug if debug is not None else self.debug
-        lv = ListenerView(callback=self.callback,
-                          get_response=self.get_response)
+
+        # Setup the app and add the custom URL rule for the listener.
         self.app = Flask(__name__)
         self.app.add_url_rule(self.listener_url,
-                              view_func=lv.as_view(
-                                  self.listener_url.replace('/', '')
+                              view_func=ListenerView.as_view(
+                                  self.listener_url.replace('/', ''),
+                                  callback=self.callback,
+                                  get_response=self.get_response
                               )
                               )
         self.app.run(
@@ -45,22 +77,38 @@ class RTUListener(object):
 
 class ListenerView(View):
     """
-    Main class for handling listening and responding.
+    Main class for handling listening and responding. The default GET response
+    is 'hello'.
     """
 
     methods = ['GET', 'POST']
     get_response = 'hello'
 
     def __init__(self, callback=None, get_response=None):
-        # TODO: These are not sticking
         self.callback = callback or self.default_callback
         self.get_response = get_response or self.get_response
 
     def dispatch_request(self):
+        """
+        This must be here for the Flask View to work. We verify that we got POST
+        data and send it to the callback function, otherwise we assume it was a
+        GET and respond with the configured GET response.
+        """
+
         if request.method == 'POST':
-            return self.callback(response='foo')
+            return self.callback(request=request.get_json(force=True))
         return self.get_response
 
-    def default_callback(self, response):
-        print 'Response: %s' % response
+    def default_callback(self, request):
+        """
+        Basic default callback if no other callback is defined. We consume the
+        request and print it for viewing. We have to return something here for
+        the Flask listener to work and respond to the client request. If an
+        issue occurs prior to the '200 OK' response, Flask handles it.
+
+        :param request: The request data.
+        :type request: dict
+        """
+
+        print 'POST Data: {}'.format(request)
         return '200 OK'
