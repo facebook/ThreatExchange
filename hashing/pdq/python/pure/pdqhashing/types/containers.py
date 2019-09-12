35b96818fdd1f5c9816c1b8fd5caa5cdd44a25b0
John Kerl <kerl.john.r@gmail.com>
2019-09-12 15:49:00 -0400

    pure-py

---
+++ hashing/pdq/python/pure/pdqhashing/types/containers.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+
+class Hash256AndMetadata:
+    """ Container for MIH queries """
+
+    def __init__(self, hash, metadata) -> None:
+        self.hash = hash
+        self.metadata = metadata
+
+
+class HashAndQuality:
+    """ Container for multiple-value object: the hash is a 64-character hex
+    string and the quality is an integer in the range 0..100."""
+
+    def __init__(self, hash, quality) -> None:
+        self.hash = hash
+        self.quality = quality
+
+    def getHash(self):
+        return self.hash
+
+    def getQuality(self):
+        return self.quality
+
+
+class HashesAndQuality:
+    """ Container for multiple-value object. """
+
+    def __init__(
+        self,
+        hash,
+        hashRotate90,
+        hashRotate180,
+        hashRotate270,
+        hashFlipX,
+        hashFlipY,
+        hashFlipPlus1,
+        hashFlipMinus1,
+        quality,
+    ) -> None:
+
+        self.hash = hash
+        self.hashRotate90 = hashRotate90
+        self.hashRotate180 = hashRotate180
+        self.hashRotate270 = hashRotate270
+        self.hashFlipX = hashFlipX
+        self.hashFlipY = hashFlipY
+        self.hashFlipPlus1 = hashFlipPlus1
+        self.hashFlipMinus1 = hashFlipMinus1
+        self.quality = quality
