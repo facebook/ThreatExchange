eb0b953dd320e8d7f333b7d3f6452d54cf9be649
John Kerl <kerl.john.r@gmail.com>
2020-07-08 23:01:56 -0400

    typofix sheshebangs -> shebangs

--- hashing/te-tag-query-ruby/api-example-submit.rb
+++ hashing/te-tag-query-ruby/api-example-submit.rb
@@ -1,4 +1,4 @@
-##!/usr/bin/env ruby
+#!/usr/bin/env ruby
 
 # ================================================================
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
