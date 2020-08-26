9624704b311d832984bfdd5458f2a3d557aa8846
Jesse Berliner-Sachs <jeberl@fb.com>
2020-08-26 13:16:01 -0400

    Remove \ruby top level directory

--- ruby/test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-
-require 'ThreatExchange'
-require 'minitest/autorun'
-require 'webmock/minitest'
