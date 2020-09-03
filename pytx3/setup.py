b8689cd5dd9c9292d28f1850244d1526c5f9dd1b
David Callies <dcallies@fb.com>
2020-09-03 11:48:56 -0700

    [pytx3] 0.0.8 and setup.py fixes

--- pytx3/setup.py
+++ pytx3/setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 with open(path.join(here, "DESCRIPTION.rst"), encoding="utf-8") as f:
@@ -17,9 +17,10 @@ with open(path.join(here, "DESCRIPTION.rst"), encoding="utf-8") as f:
 
 setup(
     name="pytx3",
-    version="0.0.7",
+    version="0.0.8",
     description="Python Library for Facebook ThreatExchange",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Facebook",
     author_email="threatexchange@fb.com",
     license="BSD",
