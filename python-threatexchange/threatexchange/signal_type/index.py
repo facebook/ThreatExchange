706749335d6ceeaf7d8277e69a70b4ba4709803c
David Callies <dcallies@fb.com>
2021-02-23 10:22:03 -0800

    moar comments

--- python-threatexchange/threatexchange/signal_type/index.py
+++ python-threatexchange/threatexchange/signal_type/index.py
@@ -27,6 +27,16 @@ T = t.TypeVar("T")
 
 
 class IndexMatch(t.Generic[T]):
+    """
+    Wrapper around a match to the index, which may or may not
+    be an actual match based on the distance settings for a source.
+
+    The index is free to optimize not returning values that are
+    greater than would be considered matches by a downstream (i.e.
+    31 is probably the max distance that would be used in PDQ)
+
+    NamedTuple can't be made generic, so here's an equivalent
+    """
 
     __slots__ = ["distance", "metadata"]
     distance: int
@@ -46,9 +56,18 @@ class SignalTypeIndex(t.Generic[T]):
     The T can be whatever metadata might be useful, although be warned
     that not all T may serialize correctly.
 
+    # Why is `hash` str?
+    There are various multi-pass hashing approaches that may generate more
+    complicated data structures, or require multiple query passes. It's
+    unclear whether we should try and generalize those yet, and forcing the
+    type to always be serializable in str gives us the advantage that
+    uploading it to ThreatExchange is always straightforward.
+
+    # Handling Restricted Value Types
     In cases where your underlying index has limited type support for
-    values, the easiest workaround is to just generate your own
-    internal ids and maintain a separate map of the real results.
+    values (i.e. FAISS seems to only support ints), the easiest workaround
+    is to just generate your own internal ids and maintain a separate map of
+    the real results.
 
     fancy_index_only_to_int[str, int]
     internal_mapping List[T] or Dict[int, T]
@@ -69,6 +88,10 @@ class SignalTypeIndex(t.Generic[T]):
     def query(self, hash: str) -> t.List[IndexMatch[T]]:
         """
         Look up entries against the index, up to the max supported distance.
+
+        The index is free to optimize not returning values that are
+        greater than would be considered matches by a downstream system
+        (i.e. 31 is probably the max distance that would be used in PDQ)
         """
         raise NotImplementedError
 
@@ -87,7 +110,15 @@ class SignalTypeIndex(t.Generic[T]):
         raise NotImplementedError
 
     def serialize(self, fout: t.BinaryIO) -> None:
-        """Convert the index into a bytestream (probably a file)"""
+        """
+        Convert the index into a bytestream (probably a file).
+
+        The reason to use an IO instead of just stringifying it is that some
+        indices might be really big, and we don't want to pay a cost of 2x-ing
+        our memory storage (versus writing to file).
+
+        Could also be premature optimization, you decide!
+        """
         raise NotImplementedError
 
     @classmethod
