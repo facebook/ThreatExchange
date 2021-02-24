6dd93968716255005d29fe72883dcc5a9588fbb8
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-02-24 15:02:00 -0500

    [HMA][Tests] Add scaffolding for py.test
    Summary
    ---
    Multiple developers are writing tests at the moment. Would help
    to have some common ground.
    
    Test Plan
    ---
    ```
    $> pip install -r requirements-dev.txt
    $> py.test
    ```
    
    Produces some output and fails.
    
    TODO:
    ---
    Add makefile target and github workflow.

---
+++ hasher-matcher-actioner/tests/__init__.py
@@ -0,0 +1,6 @@
+# Do not remove. This makes it possible for modules to reference hmalib.
+# Unless tests is a module, you'll have to do PYTHONPATH wizardry to get
+# ```
+# from hmalib import foo
+# ```
+# to work.
