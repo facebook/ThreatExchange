f492537f096874df094cfbf0b30b1c649df190bb
Barrett Olson <barrett@fb.com>
2021-02-26 12:54:07 -0500

    pdq_index quick fixes

--- python-threatexchange/threatexchange/signal_type/pdq_index.py
+++ python-threatexchange/threatexchange/signal_type/pdq_index.py
@@ -1,17 +1,15 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
 """
-Implementation of SignalTypeIndex abstraction for PDQ by wrapping hashing.pdq_faiss_matcher.
+Implementation of SignalTypeIndex abstraction for PDQ by wrapping 
+hashing.pdq_faiss_matcher.
 """
 
 import typing as t
 import pickle
 
-from threatexchange.signal_type.index import SignalTypeIndex, IndexMatch
+from threatexchange.signal_type.index import SignalTypeIndex, IndexMatch, T as IndexT
 from threatexchange.hashing.pdq_faiss_matcher import PDQMultiHashIndex
-
-T = t.TypeVar("T")
-PDQ_CONFIDENT_MATCH_THRESHOLD = 31
 
 
 class PDQIndex(SignalTypeIndex):
@@ -19,13 +17,16 @@ class PDQIndex(SignalTypeIndex):
     Wrapper around the pdq faiss index lib
     """
 
+    PDQ_CONFIDENT_MATCH_THRESHOLD = 31
+    T = IndexT
+
     def __init__(self, entries: t.Iterable[t.Tuple[str, T]]) -> None:
         super().__init__()
-        self.local_id_to_entry = dict()
-        hashes = list()
-        for i in range(len(entries)):
-            self.local_id_to_entry[i] = entries[i]
-            hashes.append(entries[i][0])
+        self.local_id_to_entry = {}
+        hashes = []
+        for i, entry in enumerate(entries):
+            self.local_id_to_entry[i] = entry
+            hashes.append(entry[0])
         self.index = PDQMultiHashIndex.create(
             hashes, custom_ids=self.local_id_to_entry.keys()
         )
@@ -34,10 +35,12 @@ class PDQIndex(SignalTypeIndex):
         """
         Look up entries against the index, up to the max supported distance.
         """
+
+        # query takes a signal hash but index supports banch quries hence [hash]
         results = self.index.search(
-            [hash], PDQ_CONFIDENT_MATCH_THRESHOLD, return_as_ids=True
+            [hash], self.PDQ_CONFIDENT_MATCH_THRESHOLD, return_as_ids=True
         )
-        matches = list()
+        matches = []
         for result_ids in results:
             for id in result_ids:
                 # distance = -1 (index does not currently support distance)
