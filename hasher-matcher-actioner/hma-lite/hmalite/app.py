6d6448bab9b0dc49b174ca347092c06c472419c8
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-02-26 12:19:18 -0500

    Add mock APIs for matching single and multiple hashes (#381)
    Summary
    ---
    Add a flask blueprint that provides one endpoint accessible via GET or POST as detailed in API.md
    
    Test plan
    ---
    First run the application
    ```
    $> FLASK_APP=hmalite.app:app FLASK_ENV=development flask run
    ```
    
    Then run the tests. Because the matches are random, you might need to run the tests a few times to get the outputs you want.
    
    GET Request
    ```
    $> curl -v "http://localhost:5000/v1/hashes/query?hash=tyui"
    ```
    
    Note the status header.
    
    POST Requests
    ```
    $> curl --header "Content-Type: application/json" \
      --request POST \
      --data '{"hashes":["xyz", "abc"]}' \
      http://localhost:5000/v1/hashes/query
    ```

--- hasher-matcher-actioner/hma-lite/hmalite/app.py
+++ hasher-matcher-actioner/hma-lite/hmalite/app.py
@@ -1,5 +1,9 @@
 from flask import Flask
+
+from hmalite.matcher import matcher_api
+
 app = Flask(__name__)
+app.register_blueprint(matcher_api, url_prefix="/v1/hashes")
 
 @app.route('/')
 def hello_world():
