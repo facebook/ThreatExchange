369f03d41e663151b166f1edea05a3258be75154
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-03-12 16:54:07 -0500

    Support image file bytes as input to PDQ hashing (#420)
    * Support image file bytes as input to PDQ hashing
    
    Summary
    ---
    In HMA, we fetch file bytes from s3 and then hash them. Writing them to an interim temp file only to read them again is inefficient and has proven to be a source of errors when the file is not flushed after writes.
    
    This allows directly passing along bytes from the python program to the hasher.
    
    also: Tests and typing were added to existing functions.
    
    Test Plan
    ---
    ```
    $ py.test
    ```
    
    * lint: fix pdq_hasher.py
    
    * more lint. Wish it were lindt instead!

--- python-threatexchange/threatexchange/hashing/pdq_hasher.py
+++ python-threatexchange/threatexchange/hashing/pdq_hasher.py
@@ -1,20 +1,39 @@
 #!/usr/bin/env python
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+import io
 import pdqhash
 import pathlib
-import numpy as np
+import numpy
 from PIL import Image, ImageOps
+import typing as t
 
 
-def pdq_from_file(path: pathlib.Path):
+PDQOutput = t.Tuple[
+    str, int
+]  # hexadecimal representation of the Hash vector and a numerical quality value
+
+
+def pdq_from_file(path: pathlib.Path) -> PDQOutput:
     """
-    Given a path to a file return the PDQ Hash string in hex
-    Current Supported file types: jpg
+    Given a path to a file return the PDQ Hash string in hex.
+    Current tested against: jpg
     """
     img_pil = Image.open(path)
-    image = np.asarray(img_pil)
-    hash_vector, quality = pdqhash.compute(image)
+    image = numpy.asarray(img_pil)
+    return _pdq_from_numpy_array(image)
+
+
+def pdq_from_bytes(file_bytes: bytes) -> PDQOutput:
+    """
+    For the bytestream from an image file, compute PDQ Hash and quality.
+    """
+    np_array = numpy.asarray(Image.open(io.BytesIO(file_bytes)))
+    return _pdq_from_numpy_array(np_array)
+
+
+def _pdq_from_numpy_array(array: numpy.ndarray) -> PDQOutput:
+    hash_vector, quality = pdqhash.compute(array)
 
     bin_str = "".join([str(x) for x in hash_vector])
 
