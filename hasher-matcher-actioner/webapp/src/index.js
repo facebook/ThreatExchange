b9c20e4db504b1a88e7511b50e680a282a14037f
Brian Phillip Eriksen <brianeriksen@fb.com>
2021-03-16 11:16:59 -0400

    Webapp wireframe prototype v0.

--- hasher-matcher-actioner/webapp/src/index.js
+++ hasher-matcher-actioner/webapp/src/index.js
@@ -2,15 +2,13 @@
  * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
  */
 
-'use strict';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
