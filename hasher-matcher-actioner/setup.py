6bfd461d0a32bd5f1458ed1a79f9191486c6305b
David Callies <dcallies@fb.com>
2021-03-22 11:55:50 -0700

    black]

--- hasher-matcher-actioner/setup.py
+++ hasher-matcher-actioner/setup.py
@@ -7,7 +7,6 @@ from setuptools import setup
 setup(
     name="hmalib",
     description="Convenience package for hmalib. Probably don't distribute it.",
-
     install_requires=[
         "boto3",
         "boto3-stubs[essential]==1.17.14.0",
