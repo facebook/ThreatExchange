017beddfc5750676b89716b27de6edabfe84bdf1
Mengyang Wang <81996660+mengyangwang@users.noreply.github.com>
2021-04-13 11:45:21 -0400

    Update DynamoDB Config table and fetcher Lambda to allow configs to b… (#481)
    * Update DynamoDB Config table and fetcher Lambda to allow configs to be turned on or off
    
    * update data type to boolean
    
    * update active to a meaningful name
    
    * Update fetcher.py
    
    * nit grammar
    
    Co-authored-by: Mengyang Wang <wangmengyang@wangmengyang-mbp.dhcp.thefacebook.com>

--- hasher-matcher-actioner/hmalib/lambdas/fetcher.py
+++ hasher-matcher-actioner/hmalib/lambdas/fetcher.py
@@ -66,14 +66,17 @@ def lambda_handler(event, context):
 
     response_iterator = paginator.paginate(
         TableName=config.collab_config_table,
-        ProjectionExpression=",".join(("#Name", "privacy_group", "tags")),
+        ProjectionExpression=",".join(
+            ("#Name", "privacy_group", "tags", "fetcher_active")
+        ),
         ExpressionAttributeNames={"#Name": "Name"},
     )
 
     collabs = []
     for page in response_iterator:
         for item in page["Items"]:
-            collabs.append((item["Name"], item["privacy_group"]))
+            if item["fetcher_active"]:
+                collabs.append((item["Name"], item["privacy_group"]))
 
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
