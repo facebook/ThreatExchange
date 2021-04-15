afa2d9614d396b1c3f17a2a8db2cc53ed78e35a2
Jesse Berliner-Sachs <jeberl@fb.com>
2021-04-15 14:45:20 -0400

    mock action performer configs (#493)
    * mock action performer configs
    
    * small refactor
    
    * mypy imports
    
    * Make work in aws
    
    * add tests and fix black
    
    * Brians comments and make fucntion static

--- hasher-matcher-actioner/hmalib/common/actioner_models.py
+++ hasher-matcher-actioner/hmalib/common/actioner_models.py
@@ -3,7 +3,9 @@
 from dataclasses import dataclass
 import typing as t
 
-from hmalib.models import Label
+from hmalib.models import Label, MatchMessage
+
+from requests import get, post, put, delete, Response
 
 
 class LabelWithConstraints(Label):
@@ -15,6 +17,9 @@ class LabelWithConstraints(Label):
 
 class ActionLabel(LabelWithConstraints):
     _KEY_CONSTRAINT = "Action"
+
+    def __hash__(self) -> int:
+        return self.value.__hash__()
 
 
 class ThreatExchangeReactionLabel(LabelWithConstraints):
@@ -33,3 +38,54 @@ class ActionRule:
     action_label: ActionLabel
     must_have_labels: t.List[Label]
     must_not_have_labels: t.List[Label]
+
+
+TUrl = t.Union[t.Text, bytes]
+
+
+class HTTPRequest:
+    @staticmethod
+    def call(url: TUrl, **kwargs) -> Response:
+        raise NotImplementedError()
+
+
+class Post(HTTPRequest):
+    @staticmethod
+    def call(url: TUrl, **kwargs) -> Response:
+        data = kwargs.get("data")
+        return post(url, data)
+
+
+class Get(HTTPRequest):
+    @staticmethod
+    def call(url: TUrl, **kwargs) -> Response:
+        return get(url)
+
+
+class Put(HTTPRequest):
+    @staticmethod
+    def call(url: TUrl, **kwargs) -> Response:
+        data = kwargs.get("data")
+        return put(url, data)
+
+
+class Delete(HTTPRequest):
+    @staticmethod
+    def call(url: TUrl, **kwargs) -> Response:
+        return delete(url)
+
+
+class ActionPerformer:
+    def perform_action(self, match_message: MatchMessage) -> None:
+        raise NotImplementedError()
+
+
+@dataclass
+class WebhookActionPerformer(ActionPerformer):
+
+    request_type: t.Type[HTTPRequest]
+    url: TUrl
+
+    def perform_action(self, match_message: MatchMessage) -> None:
+        kwargs = {"url": self.url, "data": match_message.to_sns_message()}
+        self.request_type.call(**kwargs)
