6bb4ea5861338c28e296c0b260281e97bf8a3f9a
Barrett Olson <barrett@fb.com>
2021-04-20 13:58:09 -0400

    have hma require pytx 0.0.18

--- hasher-matcher-actioner/setup.py
+++ hasher-matcher-actioner/setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         "boto3",
         "boto3-stubs[essential,sns]==1.17.14.0",
-        "threatexchange[faiss,pdq_hasher]>=0.0.15",
+        "threatexchange[faiss,pdq_hasher]>=0.0.18",
         "bottle",
         "apig_wsgi",
     ],
