3b272a9a19b437dfae374c8d85165389e94abdf8
Brian Eriksen <eriksendc@yahoo.com>
2021-04-28 17:58:51 -0400

    Added routes / paths for each settings tab. (Rebase.)

--- hasher-matcher-actioner/webapp/src/Settings.jsx
+++ hasher-matcher-actioner/webapp/src/Settings.jsx
@@ -5,15 +5,36 @@
 import React from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import {useHistory, useParams} from 'react-router-dom';
 import ActionSettingsTab from './settings/ActionSettingsTab';
 import ThreatExchangeSettingsTab from './settings/ThreatExchangeSettingsTab';
 
 export default function Settings() {
+  const {tab} = useParams();
+  const history = useHistory();
+  if (
+    tab === undefined ||
+    !tab ||
+    (tab !== 'threatexchange' &&
+      tab !== 'signals' &&
+      tab !== 'pipeline' &&
+      tab !== 'actions')
+  ) {
+    window.location = '/settings/threatexchange';
+  }
   return (
     <>
-      <Tabs defaultActiveKey="threatexchange" id="setting-tabs">
+      <Tabs
+        activeKey={tab}
+        id="setting-tabs"
+        onSelect={key => {
+          history.push(`/settings/${key}`);
+        }}>
         <Tab eventKey="threatexchange" title="ThreatExchange">
           <ThreatExchangeSettingsTab />
+        </Tab>
+        <Tab eventKey="signals" title="Signals">
+          <SignalSettingsTab />
         </Tab>
         <Tab eventKey="pipeline" title="Pipeline">
           Todo!
