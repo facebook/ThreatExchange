9cf58d391eb4b434a7b6bb1ce12aa46117ca1ac5
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-05-11 17:54:25 +0000

    Add privacy-group information to copydays blurb

--- hasher-matcher-actioner/webapp/src/components/CopydaysInformationBlock.jsx
+++ hasher-matcher-actioner/webapp/src/components/CopydaysInformationBlock.jsx
@@ -15,11 +15,21 @@ export default function CopydaysInformationBlock() {
       <p>
         In addition to ThreatExchange data, HMA also comes with a set of image
         hashes baked in. The images are from a dataset called the Copydays
-        dataset. Go to{' '}
-        <a href="https://lear.inrialpes.fr/~jegou/data.php">this page</a> and
-        find the link labeled <code>jpg1.tar.gz</code>. Any photo from that
-        archive file will match in the HMA system.
+        dataset.
       </p>
+
+      <ul>
+        <li>
+          Go to{' '}
+          <a href="https://lear.inrialpes.fr/~jegou/data.php">this page</a> and
+          find the link labeled <code>jpg1.tar.gz</code>. Any photo from that
+          archive file will match in the HMA system.
+        </li>
+        <li>
+          The matched photos will have a privacy group of{' '}
+          <code>copydays-test</code>.
+        </li>
+      </ul>
     </Card>
   );
 }
