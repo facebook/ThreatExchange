ff4c1d0678e3f2a663a9ae06d26aafe9629ce707
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-05-12 22:04:12 +0000

    Move shortenNumRepr into its own module

--- hasher-matcher-actioner/webapp/src/components/GraphWithNumberWidget.jsx
+++ hasher-matcher-actioner/webapp/src/components/GraphWithNumberWidget.jsx
@@ -8,19 +8,7 @@ import PropTypes from 'prop-types';
 // import uPlot from 'uplot';
 import UplotReact from 'uplot-react';
 import '../../node_modules/uplot/dist/uPlot.min.css';
-
-/**
- * Reduces the width required for displaying a number. eg. 4,000 -> 4k.
- * 2,000,000 -> 2m etc.
- */
-function shortenNumRepr(n) {
-  if (n < 1e3) return n;
-  if (n >= 1e3 && n < 1e6) return `${+(n / 1e3).toFixed(1)}k`;
-  if (n >= 1e6 && n < 1e9) return `${+(n / 1e6).toFixed(1)}m`;
-  if (n >= 1e9 && n < 1e12) return `${+(n / 1e9).toFixed(1)}b`;
-
-  return `${+(n / 1e12).toFixed(1)}t`;
-}
+import shortenNumRepr from '../utils/NumberUtils';
 
 const opts = {
   id: 'chart1',
