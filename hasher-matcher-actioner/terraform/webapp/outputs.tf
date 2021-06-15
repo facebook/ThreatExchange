d9286d68e6eec6fe35288be71debd7f5fc1ccca6
Jesse Berliner-Sachs <jeberl@fb.com>
2021-06-15 11:26:05 -0500

    Add UI URL to terraform outputs

--- hasher-matcher-actioner/terraform/webapp/outputs.tf
+++ hasher-matcher-actioner/terraform/webapp/outputs.tf
@@ -7,3 +7,7 @@ output "s3_bucket_name" {
 output "cloudfront_distribution_domain_name" {
   value = var.include_cloudfront_distribution ? aws_cloudfront_distribution.webapp[0].domain_name : "no-cloudfront-distribution"
 }
+
+output "ui_url" {
+  value = aws_s3_bucket.webapp.website_endpoint
+}
