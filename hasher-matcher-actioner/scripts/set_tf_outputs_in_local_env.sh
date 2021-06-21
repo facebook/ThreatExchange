c23a48b08159732c9a6805857d66e20b1f50bb44
Barrett Olson <barrett@fb.com>
2021-06-21 18:27:06 +0000

    add retry with exponential backoff

--- hasher-matcher-actioner/scripts/set_tf_outputs_in_local_env.sh
+++ hasher-matcher-actioner/scripts/set_tf_outputs_in_local_env.sh
@@ -5,6 +5,7 @@
 # usage:
 #    $ source scripts/set_tf_outputs_in_local_env.sh 
 
+echo "Reminder: only works if you 'source' the file"
 for s in $(terraform -chdir=terraform output -json | jq -r "to_entries|map(\"HMA_\(.key|ascii_upcase)=\(.value.value|tostring)\")|.[]"); do
     export $s
 done 
