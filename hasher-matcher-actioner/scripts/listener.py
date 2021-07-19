871832c2b95e2fb05fcb15afd7bb8c03df1fae11
Barrett Olson <barrett@fb.com>
2021-07-19 12:43:32 -0400

    [hma][scripts] Add latency stats and refactor soak test (#718)
    * move submitting thread to own file and add latency command
    
    * add readme and have timestamps added to file
    
    * move -time- divider for easier clean up

--- hasher-matcher-actioner/scripts/listener.py
+++ hasher-matcher-actioner/scripts/listener.py
@@ -3,7 +3,7 @@
 
 import os
 import json
-import time
+import datetime
 import threading
 import requests
 import typing as t
@@ -18,6 +18,11 @@ class _Handler(BaseHTTPRequestHandler):
 
     count_lock: threading.Lock = threading.Lock()
     post_counter: int = 0
+    latency_lock: threading.Lock = threading.Lock()
+    # ToDo do we want this to grow without bound or be limted to x most recent items?
+    submission_latencies: t.List[
+        t.Tuple[datetime.datetime, datetime.datetime, float]
+    ] = []
 
     def _set_response(self):
         self.send_response(200)
@@ -53,6 +58,20 @@ class _Handler(BaseHTTPRequestHandler):
             _Handler.post_counter += 1
         self._set_response()
         self.wfile.write(json.dumps(payload).encode("utf-8"))
+
+        present = datetime.datetime.now()
+        if content_id := json.loads(post_data).get("content_key"):
+            try:
+                submit_time = datetime.datetime.fromisoformat(
+                    content_id.split("-time-")[1]
+                )
+                latency = present - submit_time
+                with self.latency_lock:
+                    self.submission_latencies.append(
+                        (submit_time, present, latency.total_seconds())
+                    )
+            except ValueError and IndexError:
+                pass
 
 
 class Listener:
@@ -93,6 +112,12 @@ class Listener:
             return 0
         with _Handler.count_lock:
             return _Handler.post_counter
+
+    def get_submission_latencies(
+        self,
+    ) -> t.List[t.Tuple[datetime.datetime, datetime.datetime, float]]:
+        with _Handler.latency_lock:
+            return _Handler.submission_latencies.copy()
 
 
 if __name__ == "__main__":
