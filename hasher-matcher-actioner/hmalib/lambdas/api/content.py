291502c3fd04b51f9aa040f355a52c23b03d59d5
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-08-18 15:43:06 +0000

    Add a single API for progress of a content through the pipeline
    Flaws:
    - No records created when action_evaluator emits actions to be performed
    - No records created when any of the stages fail to produce an output
    
    This covers happy actionable flows only. We'll need to make changes throughout the optional lambdas to support the flawed cases.

--- hasher-matcher-actioner/hmalib/lambdas/api/content.py
+++ hasher-matcher-actioner/hmalib/lambdas/api/content.py
@@ -2,17 +2,18 @@
 
 import bottle
 import boto3
-
-from enum import Enum
+from datetime import datetime
 from dataclasses import dataclass, asdict, field
 from mypy_boto3_dynamodb.service_resource import Table
 from boto3.dynamodb.conditions import Attr, Key, Or
 from botocore.exceptions import ClientError
 import typing as t
 
+from threatexchange.signal_type.signal_base import SignalType
+from threatexchange.content_type.content_base import ContentType
 
 from hmalib.lambdas.api.middleware import jsoninator, JSONifiable, DictParseable
-from hmalib.models import PipelineHashRecord
+from hmalib.models import MatchRecord, PipelineHashRecord
 from hmalib.common.content_models import (
     ContentObject,
     ActionEvent,
@@ -34,6 +35,54 @@ class HashResultResponse(JSONifiable):
 
     def to_json(self) -> t.Dict:
         return asdict(self)
+
+
+@dataclass
+class ContentPipelineProgress(JSONifiable):
+    """
+    Encompasses optional results for all stages of the pipeline. Includes
+    results from any of those stages and the time those stages were updated.
+    """
+
+    content_id: str
+    content_type: t.Type[ContentType]
+    content_preview_url: str
+
+    # Stage update times
+    submitted_at: t.Optional[datetime]
+    hashed_at: t.Optional[datetime] = None
+    matched_at: t.Optional[datetime] = None
+    action_evaluated_at: t.Optional[datetime] = None
+    action_performed_at: t.Optional[datetime] = None
+
+    # Stage update results
+    submission_additional_fields: t.List[str] = field(default_factory=list)
+
+    # map from signal_type string => hash value
+    hash_results: t.Dict[str, str] = field(default_factory=dict)
+
+    # map from signal_id => set[classification_strings]
+    match_results: t.Dict[str, t.List[str]] = field(default_factory=dict)
+
+    # list of action names that must be performed
+    action_evaluation_results: t.List[str] = field(default_factory=list)
+
+    # list of action names that have been performed.
+    action_perform_results: t.List[str] = field(default_factory=list)
+
+    def to_json(self) -> t.Dict:
+        result = asdict(self)
+        result.update(
+            content_type=self.content_type.get_name(),
+            submitted_at=self.submitted_at and self.submitted_at.isoformat(),
+            hashed_at=self.hashed_at and self.hashed_at.isoformat(),
+            matched_at=self.matched_at and self.matched_at.isoformat(),
+            action_evaluated_at=self.action_evaluated_at
+            and self.action_evaluated_at.isoformat(),
+            action_performed_at=self.action_performed_at
+            and self.action_performed_at.isoformat(),
+        )
+        return result
 
 
 @dataclass
@@ -68,6 +117,60 @@ def get_content_api(
         if content_id:
             return ContentObject.get_from_content_id(dynamodb_table, content_id)
         return None
+
+    @content_api.get("/pipeline-progress/", apply=[jsoninator])
+    def pipeline_progress() -> ContentPipelineProgress:
+        """
+        Build a history of the stages that this piece of content has gone
+        through and what their results were. Do not call this from anything but
+        a UI. This is not optimized for performance.
+        """
+        content_id = bottle.request.query.content_id or None
+        content_object = ContentObject.get_from_content_id(dynamodb_table, content_id)
+        preview_url = content_object.content_ref
+
+        # The result object will be gradually built up as records are retrieved.
+        result = ContentPipelineProgress(
+            content_id=content_id,
+            content_type=content_object.content_type,
+            content_preview_url=preview_url,
+            submitted_at=content_object.updated_at,
+            submission_additional_fields=list(content_object.additional_fields),
+        )
+
+        hash_records = PipelineHashRecord.get_from_content_id(
+            dynamodb_table, content_id
+        )
+        if len(hash_records) != 0:
+            result.hashed_at = min(hash_records, key=lambda r: r.updated_at).updated_at
+            for hash_record in hash_records:
+                # Assume that each signal type has a single hash
+                result.hash_results[
+                    hash_record.signal_type.get_name()
+                ] = hash_record.content_hash
+
+        match_records = MatchRecord.get_from_content_id(dynamodb_table, content_id)
+        if len(match_records) != 0:
+            result.matched_at = min(
+                match_records, key=lambda r: r.updated_at
+            ).updated_at
+
+            # TODO #751 Until we resolve type agnostic storage of signal data,
+            # we can't populate match details.
+            # actually populate result.match_results.
+
+        # TODO: ActionEvaluation does not yet leave a trail. Either record
+        # action evaluation or remove the evaluation stage from the
+        # pipeline-progress indicator.
+
+        action_records = ActionEvent.get_from_content_id(dynamodb_table, content_id)
+        if len(action_records) != 0:
+            result.action_performed_at = min(
+                action_records, key=lambda r: r.performed_at
+            ).performed_at
+            result.action_perform_results = [r.action_label for r in action_records]
+
+        return result
 
     @content_api.get("/action-history/", apply=[jsoninator])
     def action_history() -> ActionHistoryResponse:
