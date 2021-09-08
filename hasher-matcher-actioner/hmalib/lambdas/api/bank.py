87f17a4c3a32cd09aebb694158227880ac5e895b
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-09-08 21:47:31 +0000

    API to create, get-all and get banks

--- hasher-matcher-actioner/hmalib/lambdas/api/bank.py
+++ hasher-matcher-actioner/hmalib/lambdas/api/bank.py
@@ -4,6 +4,12 @@ import bottle
 
 from mypy_boto3_dynamodb.service_resource import Table
 
+from hmalib.common.messages.banks_api import AllBanksEnvelope
+from hmalib.common.models.bank import Bank, BanksTable
+
+from .middleware import jsoninator
+
+
 def get_bank_api(bank_table: Table) -> bottle.Bottle:
     """
     Closure for dependencies of the bank API
@@ -11,5 +17,21 @@ def get_bank_api(bank_table: Table) -> bottle.Bottle:
 
     bank_api = bottle.Bottle()
     table_manager = BanksTable(table=bank_table)
+
+    @bank_api.get("/get-all-banks", apply=[jsoninator])
+    def get_all_banks() -> AllBanksEnvelope:
+        return AllBanksEnvelope(banks=table_manager.get_all_banks())
+
+    @bank_api.get("/get-bank/<bank_id>", apply=[jsoninator])
+    def get_bank(bank_id=None) -> Bank:
+        bank = table_manager.get_bank(bank_id=bank_id)
+        return bank
+
+    @bank_api.post("/create-bank", apply=[jsoninator])
+    def create_bank() -> Bank:
+        return table_manager.create_bank(
+            bank_name=bottle.request.json["bank_name"],
+            bank_description=bottle.request.json["bank_description"],
+        )
 
     return bank_api
