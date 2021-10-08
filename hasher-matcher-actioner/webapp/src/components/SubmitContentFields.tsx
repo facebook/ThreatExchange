c87634b7b88c1ebf33751336bd303ab289e59f10
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-10-08 14:51:07 +0000

    Revamp submit page
    - [forward looking] use hma-themed-form to bring ui language in line with other forms in banks.
    - remove obsolete warning message
    - make the photo/video choice visible and prominent (During demos, I often miss correcting to video) (reduces #clicks required from 2 -> 1
    - Dedicated large touch area radio buttons: <ChoiceCard/>
    - remove the ContentIdUniqueField since it was broken up
    - Will revisit additional fields in a separate diff

--- hasher-matcher-actioner/webapp/src/components/SubmitContentFields.tsx
+++ hasher-matcher-actioner/webapp/src/components/SubmitContentFields.tsx
@@ -27,53 +27,6 @@ type ContentFieldProps = {
   handleInputChange: (e: React.SyntheticEvent) => void;
 };
 
-export function ContentUniqueIdField({
-  inputs,
-  handleInputChange,
-}: ContentFieldProps): JSX.Element {
-  return (
-    <Form.Group>
-      <Form.Row>
-        <Form.Label>Unique ID for Content</Form.Label>
-        <InputGroup>
-          <Form.Control
-            onChange={handleInputChange}
-            type="text"
-            name="contentId"
-            placeholder="Enter a unique identifier for content"
-            required
-            value={inputs.contentId}
-          />
-          <Form.Group className="mb-0">
-            <Form.Control
-              onChange={handleInputChange}
-              required
-              as="select"
-              name="contentType"
-              value={inputs.contentType}
-              custom>
-              {/* (defaults to photo for now) */}
-              <option key="empty" value="" hidden>
-                Select content type...
-              </option>
-              {/* TODO: Use an enum here. */}
-              <option key="1" value="photo">
-                Photo
-              </option>
-              <option key="2" value="video">
-                Video
-              </option>
-            </Form.Control>
-          </Form.Group>
-        </InputGroup>
-      </Form.Row>
-      <Form.Text className="text-muted mt-0">
-        Warning currently behavior will overwrite content with the same id
-      </Form.Text>
-    </Form.Group>
-  );
-}
-
 export function PhotoUploadField({
   inputs,
   handleInputChange,
@@ -142,7 +95,7 @@ export function OptionalAdditionalFields({
   return (
     <Form.Group>
       <Form.Row>
-        <Form.Label as={Col}>Optional Additional Fields</Form.Label>
+        <Form.Label>Optional Additional Fields</Form.Label>
       </Form.Row>
       {Object.keys(additionalFields).map(entry => (
         <Form.Row key={entry}>
