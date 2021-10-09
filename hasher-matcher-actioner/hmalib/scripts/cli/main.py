4d102863b12054ed34f5783ddc40cdd0caf6e535
Barrett Olson <b@barrettolson.com>
2021-10-09 18:04:57 +0000

    move storm script to hmacli command storm and add support for hash, url, and s3

--- hasher-matcher-actioner/hmalib/scripts/cli/main.py
+++ hasher-matcher-actioner/hmalib/scripts/cli/main.py
@@ -18,6 +18,7 @@ import hmalib.scripts.common.utils as utils
 
 import hmalib.scripts.cli.command_base as base
 import hmalib.scripts.cli.soak as soak
+import hmalib.scripts.cli.storm as storm
 import hmalib.scripts.cli.shell as shell
 from hmalib.scripts.cli import run_api, run_lambda
 
@@ -27,6 +28,7 @@ TERRAFORM_OUTPUTS_CACHE = "/tmp/hma-terraform-outputs.json"
 def get_subcommands() -> t.List[t.Type[base.Command]]:
     return [
         soak.SoakCommand,
+        storm.StormCommand,
         shell.ShellCommand,
         run_lambda.RunLambdaCommand,
         run_api.RunAPICommand,
@@ -88,7 +90,8 @@ def execute_command(namespace) -> None:
             # Init Values
             api = utils.HasherMatcherActionerAPI(
                 get_api_url(
-                    namespace.api_endpoint, refresh=namespace.refresh_tf_outputs
+                    cli_option=namespace.api_endpoint,
+                    refresh=namespace.refresh_tf_outputs,
                 ),
                 api_token=get_access_token(namespace.access_token),
             )
@@ -147,7 +150,7 @@ def get_api_url(cli_option: str = None, refresh=False) -> str:
             "  * a cli argument (-e)\n"
             f"  * in the environment as {environment_var}\n"
         )
-        tf_outputs = utils.get_terraform_outputs(refresh)
+        tf_outputs = get_terraform_outputs(refresh)
         url = tf_outputs["api_url"]
     return url
 
