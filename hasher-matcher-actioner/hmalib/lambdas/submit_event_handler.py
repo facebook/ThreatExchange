40a6f7998151c1da151bd2944d8b53bf3ce12546
Barrett Olson <b@barrettolson.com>
2021-10-20 19:43:41 +0000

    allow submit_event_handler to accept urls as well as s3 bucket:key pairs

--- hasher-matcher-actioner/hmalib/lambdas/submit_event_handler.py
+++ hasher-matcher-actioner/hmalib/lambdas/submit_event_handler.py
@@ -5,6 +5,7 @@ import json
 import boto3
 import typing as t
 from dataclasses import dataclass
+from mypy_boto3_dynamodb.service_resource import Table
 
 from threatexchange.content_type.content_base import ContentType
 from threatexchange.content_type.meta import (
@@ -14,7 +15,10 @@ from threatexchange.content_type.meta import (
 from hmalib.common.logging import get_logger
 from hmalib.lambdas.api.submit import (
     submit_content_request_from_s3_object,
+    record_content_submission,
+    send_submission_to_url_queue,
     SubmitContents3ObjectRequestBody,
+    SubmitContentViaURLRequestBody,
 )
 
 
@@ -25,13 +29,53 @@ DYNAMODB_TABLE = os.environ["DYNAMODB_TABLE"]
 SUBMISSIONS_QUEUE_URL = os.environ["SUBMISSIONS_QUEUE_URL"]
 
 
+class SNSSubmissionRequestBase:
+    """
+    Base class for submission request over SNS
+    """
+
+    ALWAYS_REQUIRED_FIELDS = {"content_id", "content_type"}
+    REQUIRED_SUBFIELDS: set = set()
+
+    @classmethod
+    def could_be(cls, message: t.Dict) -> bool:
+        """Check is the required fields for this class are in the message
+        Note: it checks if the value exists not if it is valid.
+        validation is handled in try_from_messsage
+        """
+        fields = message.keys()
+        if not cls.ALWAYS_REQUIRED_FIELDS.issubset(fields):
+            return False
+        if not cls.REQUIRED_SUBFIELDS.issubset(fields):
+            return False
+        return True
+
+    @classmethod
+    def try_from_messsage(cls, message: t.Dict):
+        """try to create object from an event message"""
+        raise NotImplementedError()
+
+    def submit(
+        self,
+        dynamodb_table: Table,
+        submissions_queue_url: str,
+    ):
+        """try to submit this request object to HMA"""
+        raise NotImplementedError()
+
+
 @dataclass
-class SubmissionRequest(SubmitContents3ObjectRequestBody):
+class SubmissionRequestViaS3(
+    SNSSubmissionRequestBase, SubmitContents3ObjectRequestBody
+):
     """
     Submission payload body sent via this topic is expected to be in
-    the same form as API request to the /submit/s3/ endpoint
-    with some additional validation to check the values are not empty.
+    the same form as API request to the /submit/s3/ endpoint.
+    This class adds some additional validation to check the values are not empty.
+    and handles the specific of submission
     """
+
+    REQUIRED_SUBFIELDS = {"bucket_name", "object_key"}
 
     @classmethod
     def try_from_messsage(cls, message: t.Dict):
@@ -51,35 +95,105 @@ class SubmissionRequest(SubmitContents3ObjectRequestBody):
             raise ValueError("Empty string given for required field")
         return submission
 
+    def submit(
+        self,
+        dynamodb_table: Table,
+        submissions_queue_url: str,
+    ):
+        """try to submit this request object to HMA"""
+
+        # Reminder the following method does the following:
+        # - Creates (or updates) the content object for the given content_id
+        # - Takes bucket and key -> creates a signed URL -> sends URL to hasher
+        submit_content_request_from_s3_object(
+            dynamodb_table=dynamodb_table,
+            submissions_queue_url=submissions_queue_url,
+            bucket=self.bucket_name,
+            key=self.object_key,
+            content_id=self.content_id,
+            content_type=self.content_type,
+            additional_fields=set(self.additional_fields)
+            if self.additional_fields
+            else set(),
+            force_resubmit=True,  # without this after first failure/throttle lambda is likely to get stuck in a retry loop.
+        )
+        logger.info(
+            f"Submitted to HMA - id:{self.content_id}, bucket:{self.bucket_name}, key:{self.object_key}"
+        )
+
+
+@dataclass
+class SubmissionRequestViaURL(SNSSubmissionRequestBase, SubmitContentViaURLRequestBody):
+    """
+    Submission payload body sent via this topic is expected to be in
+    the same form as API request to the /submit/url/ endpoint.
+    This class adds some additional validation to check the values are not empty.
+    and handles the specific of submission
+    """
+
+    REQUIRED_SUBFIELDS = {"content_url"}
+
+    @classmethod
+    def try_from_messsage(cls, message: t.Dict):
+        """try to create a submission from an event message"""
+        submission = cls(
+            content_id=message["content_id"],
+            content_type=get_content_type_for_name(message["content_type"]),
+            content_url=message["content_url"],
+            additional_fields=message.get("additional_fields", []),
+        )
+        if not submission.content_id or not submission.content_url:
+            raise ValueError("Empty string given for required field")
+        return submission
+
+    def submit(
+        self,
+        dynamodb_table: Table,
+        submissions_queue_url: str,
+    ):
+        """try to submit this request object to HMA"""
+
+        content_ref, content_ref_type = self.get_content_ref_details()
+
+        record_content_submission(
+            dynamodb_table,
+            content_id=self.content_id,
+            content_type=self.content_type,
+            content_ref=content_ref,
+            content_ref_type=content_ref_type,
+            additional_fields=set(self.additional_fields)
+            if self.additional_fields
+            else set(),
+            force_resubmit=True,  # without this after first failure/throttle lambda is likely to get stuck in a retry loop.
+        )
+
+        send_submission_to_url_queue(
+            dynamodb_table=dynamodb_table,
+            submissions_queue_url=submissions_queue_url,
+            content_id=self.content_id,
+            content_type=self.content_type,
+            url=self.content_url,
+        )
+        logger.info(f"Submitted to HMA - id:{self.content_id}, url:{self.content_url}")
+
 
 def lambda_handler(event, context):
     for sqs_record in event["Records"]:
         try:
             sqs_record_body = json.loads((sqs_record["body"]))
             message = json.loads(sqs_record_body["Message"])
-            submission = SubmissionRequest.try_from_messsage(message)
+            if SubmissionRequestViaURL.could_be(message):
+                submission = SubmissionRequestViaURL.try_from_messsage(message)
+            elif SubmissionRequestViaS3.could_be(message):
+                submission = SubmissionRequestViaS3.try_from_messsage(message)
+            else:
+                raise ValueError("Payload missing a required field")
         except ValueError as e:
-            logger.info("Failed to process submit event message.")
+            logger.exception("Failed to process submit event message.")
             logger.error(sqs_record)
-            # Logging as exceptions could cause the lambda to retry.
-            # We want to avoid this because it will just fail again and
-            # also trigger a resubmit for the other events/messages
-            logger.info(e)
             continue
 
-        # Reminder the following method does the following:
-        # - Creates (or updates) the content object for the given content_id
-        # - Takes bucket and key -> creates a signed URL -> sends URL to hasher
-        submit_content_request_from_s3_object(
+        submission.submit(
             dynamodb_table=dynamodb.Table(DYNAMODB_TABLE),
             submissions_queue_url=SUBMISSIONS_QUEUE_URL,
-            bucket=submission.bucket_name,
-            key=submission.object_key,
-            content_id=submission.content_id,
-            content_type=submission.content_type,
-            additional_fields=set(submission.additional_fields),
-            force_resubmit=True,  # without this after first failure/throttle lambda is likely to get stuck in a retry loop.
-        )
-        logger.info(
-            f"Submitted to HMA - id:{submission.content_id}, bucket:{submission.bucket_name}, key:{submission.object_key}"
         )
