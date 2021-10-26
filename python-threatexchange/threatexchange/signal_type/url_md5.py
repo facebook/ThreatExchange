ce3332e408fcfb7ac4458740f36f4c68abfd3234
GIFCT <tech@gifct.org>
2021-10-26 16:47:57 -0700

    minor bug fix - renamed hash_from_url to match the function in the signal base StrHasher

--- python-threatexchange/threatexchange/signal_type/url_md5.py
+++ python-threatexchange/threatexchange/signal_type/url_md5.py
@@ -20,7 +20,7 @@ class UrlMD5Signal( signal_base.SimpleSignalType, signal_base.StrHasher):
     TYPE_TAG = "media_type_url"
 
     @classmethod
-    def hash_from_url(cls,url: str) -> str:
+    def hash_from_str(cls,url: str) -> str:
         url = common.normalize_url(url)
         url_hash = hashlib.md5(url)
         return url_hash.hexdigest()
