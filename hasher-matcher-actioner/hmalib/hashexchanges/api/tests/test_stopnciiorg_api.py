970b4b1713a1db9924b774627c736e0500aceee5
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-11-30 09:33:13 -0500

    Rename NonThreatExchange* to StopNciiOrg* and make dataclasses convention friendly (#867)
    * Use pythonic styles and explicit enums for stopnciiorg APIs
    
    - also rename stopnciiorg classes from NonThreatExchange to appropriate ones
    
    * bonus: make pylance recognize python packages

--- hasher-matcher-actioner/hmalib/hashexchanges/api/tests/test_stopnciiorg_api.py
+++ hasher-matcher-actioner/hmalib/hashexchanges/api/tests/test_stopnciiorg_api.py
@@ -4,37 +4,37 @@ import os
 import unittest
 import collections.abc
 
-from hmalib.hashexchanges.api.non_threatexchange_api import NonThreatExchangeAPI
-from hmalib.hashexchanges.api.non_threatexchange_api_representations import HashRecord
+from hmalib.hashexchanges.api.stopnciiorg import StopNciiOrgAPI
+from hmalib.hashexchanges.api.stopnciiorg_representations import HashRecord
 
 X_FUNCTIONS_KEY = os.getenv("X_FUNCTIONS_KEY")
 
 OCP_APIM_SUBSCRIPTION_KEY = os.getenv("OCP_APIM_SUBSCRIPTION_KEY")
 
-BASE_NON_THREATEXCHANGE_URL = os.getenv("BASE_NON_THREATEXCHANGE_URL")
+STOPNCIIORG_BASE_URL = os.getenv("STOPNCIIORG_BASE_URL")
 
 
 @unittest.skipUnless(
-    X_FUNCTIONS_KEY and OCP_APIM_SUBSCRIPTION_KEY and BASE_NON_THREATEXCHANGE_URL,
+    X_FUNCTIONS_KEY and OCP_APIM_SUBSCRIPTION_KEY and STOPNCIIORG_BASE_URL,
     "Integration Test requires two api keys and a base url.",
 )
-class NonThreatExchangeAPIIntegrationTest(unittest.TestCase):
+class StopNciiOrgAPIIntegrationTest(unittest.TestCase):
     def setUp(self):
-        self.api = NonThreatExchangeAPI(
-            X_FUNCTIONS_KEY, OCP_APIM_SUBSCRIPTION_KEY, BASE_NON_THREATEXCHANGE_URL
+        self.api = StopNciiOrgAPI(
+            X_FUNCTIONS_KEY, OCP_APIM_SUBSCRIPTION_KEY, STOPNCIIORG_BASE_URL
         )
 
     def test_get_hashes(self):
         """
         Assumes that the response has at least one hash record.
         """
-        response = self.api.get_hash_records(start_timestamp=0, page_size=10)
+        response = self.api.get_hash_records_page(start_timestamp=0, page_size=10)
         #   test get_hash_records api from the very beginning to make sure we will get at least one record in the response.
         #   otherwise the test will fail.
         self.assertTrue(
-            isinstance(response.hashRecords, collections.abc.Sequence)
+            isinstance(response.hash_records, collections.abc.Sequence)
             and not isinstance(response, staticmethod),
             "hashRecords should be a list!",
         )
 
-        self.assertTrue(isinstance(response.hashRecords[0], HashRecord))
+        self.assertTrue(isinstance(response.hash_records[0], HashRecord))
