4ede5ea588d0294c47b586bb07ebc66d4008d83a
Barrett Olson <b@barrettolson.com>
2021-12-01 14:49:08 -0500

    fix typing and bugs in unused parts of pdq_index.py while adding flat_index option

--- python-threatexchange/threatexchange/signal_type/pdq_index.py
+++ python-threatexchange/threatexchange/signal_type/pdq_index.py
@@ -11,7 +11,11 @@ import typing as t
 import pickle
 
 from threatexchange.signal_type.index import SignalTypeIndex, IndexMatch, T as IndexT
-from threatexchange.hashing.pdq_faiss_matcher import PDQMultiHashIndex
+from threatexchange.hashing.pdq_faiss_matcher import (
+    PDQHashIndex,
+    PDQMultiHashIndex,
+    PDQFlatHashIndex,
+)
 
 
 class PDQIndex(SignalTypeIndex):
@@ -22,10 +26,15 @@ class PDQIndex(SignalTypeIndex):
     PDQ_CONFIDENT_MATCH_THRESHOLD = 31
     T = IndexT
 
-    def __init__(self, entries: t.Iterable[t.Tuple[str, T]]) -> None:
+    def __init__(
+        self, entries: t.Iterable[t.Tuple[str, IndexT]], flat_index: bool = False
+    ) -> None:
         super().__init__()
         self.local_id_to_entry: t.OrderedDict = collections.OrderedDict()
-        self.index = PDQMultiHashIndex()
+        if flat_index:
+            self.index: PDQHashIndex = PDQFlatHashIndex()
+        else:
+            self.index = PDQMultiHashIndex()
         self.add(entries=entries)
 
     def __len__(self) -> int:
@@ -33,12 +42,12 @@ class PDQIndex(SignalTypeIndex):
 
     def query(
         self, hash: str, threshhold=PDQ_CONFIDENT_MATCH_THRESHOLD
-    ) -> t.List[IndexMatch[T]]:
+    ) -> t.List[IndexMatch[IndexT]]:
         """
         Look up entries against the index, up to the max supported distance.
         """
 
-        # query takes a signal hash but index supports banch quries hence [hash]
+        # query takes a signal hash but index supports batch queries hence [hash]
         results = self.index.search([hash], threshhold, return_as_ids=True)
         matches = []
         for result_ids in results:
@@ -47,7 +56,7 @@ class PDQIndex(SignalTypeIndex):
                 matches.append(IndexMatch(-1, self.local_id_to_entry[id][1]))
         return matches
 
-    def add(self, entries: t.Iterable[t.Tuple[str, T]]) -> None:
+    def add(self, entries: t.Iterable[t.Tuple[str, IndexT]]) -> None:
         hashes = []
 
         for i, entry in enumerate(entries):
@@ -57,7 +66,9 @@ class PDQIndex(SignalTypeIndex):
         self.index.add(hashes, self.local_id_to_entry.keys())
 
     @classmethod
-    def build(cls, entries: t.Iterable[t.Tuple[str, T]]) -> "SignalTypeIndex[T]":
+    def build(
+        cls, entries: t.Iterable[t.Tuple[str, IndexT]]
+    ) -> "SignalTypeIndex[IndexT]":
         """
         Build an PDQ index from a set of entries.
         """
@@ -70,8 +81,8 @@ class PDQIndex(SignalTypeIndex):
         fout.write(pickle.dumps(self))
 
     @classmethod
-    def deserialize(cls, fin: t.BinaryIO) -> "SignalTypeIndex[T]":
+    def deserialize(cls, fin: t.BinaryIO) -> "SignalTypeIndex[IndexT]":
         """
-        Instanciate an index from a previous call to serialize
+        Instantiate an index from a previous call to serialize
         """
-        return pickle.loads(fin)
+        return pickle.loads(fin.read())
