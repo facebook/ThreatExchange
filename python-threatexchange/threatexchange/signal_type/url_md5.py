d71ea72e448749a53d9b74ddac7ab330e9c85026
Barrett Olson <b@barrettolson.com>
2021-12-01 18:17:51 -0500

    update url md5 to have correct type signature and complete docstring

--- python-threatexchange/threatexchange/signal_type/url_md5.py
+++ python-threatexchange/threatexchange/signal_type/url_md5.py
@@ -22,6 +22,6 @@ class UrlMD5Signal(signal_base.SimpleSignalType, signal_base.StrHasher):
 
     @classmethod
     def hash_from_str(cls, url: str) -> str:
-        url = common.normalize_url(url)
-        url_hash = hashlib.md5(url)
+        encoded_url = common.normalize_url(url)
+        url_hash = hashlib.md5(encoded_url)
         return url_hash.hexdigest()
