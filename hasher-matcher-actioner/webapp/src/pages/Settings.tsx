7f4f0f9b4d66b022499801bad44619b59be52dae
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-12-14 03:40:24 +0000

    UI: Index settings tab which allows rebuilding index on demand

--- hasher-matcher-actioner/webapp/src/pages/Settings.tsx
+++ hasher-matcher-actioner/webapp/src/pages/Settings.tsx
@@ -14,6 +14,7 @@ import ActionPerformerSettingsTab, {
   ActionPerformer,
 } from './settings/ActionPerformerSettingsTab';
 import ThreatExchangeSettingsTab from './settings/ThreatExchangeSettingsTab';
+import IndexSettingsTab from './settings/IndexSettingsTab';
 
 // This array must include the eventKey attribute value of any Tab in Tabs as
 // a part of the implementation to give each tab its own route.
@@ -61,6 +62,9 @@ export default function Settings(): JSX.Element {
             actionRules={actionRules}
             setActionRules={setActionRules}
           />
+        </Tab>
+        <Tab eventKey="index" title="Indexes">
+          <IndexSettingsTab />
         </Tab>
       </Tabs>
     </>
