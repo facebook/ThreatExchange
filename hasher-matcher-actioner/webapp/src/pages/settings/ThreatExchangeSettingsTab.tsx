12a665e5d9156f0a89a33cbcb4a5bf4934da7e98
Dipanjan Mukherjee <dipanjanm@fb.com>
2021-12-20 02:19:32 +0000

    Allow updating TE Token from ThreatExchange settings page
    - includes empty state for when privacy groups have not been synced.
    - removes card border around the test dataset because it looked weird as we added more settings
    - Uses <SettingsTabPane> at par with most other settings pages

--- hasher-matcher-actioner/webapp/src/pages/settings/ThreatExchangeSettingsTab.tsx
+++ hasher-matcher-actioner/webapp/src/pages/settings/ThreatExchangeSettingsTab.tsx
@@ -25,6 +25,29 @@ import {
   deleteDataset,
 } from '../../Api';
 import {NotificationsContext} from '../../AppWithNotifications';
+import SettingsTabPane from './SettingsTabPane';
+import ThreatExchangeTokenEditor from './ThreatExchangeTokenEditor';
+
+/**
+ * Empty state when no datasets are found in the backend.
+ */
+function EmptyState(): JSX.Element {
+  return (
+    <Col xs={{offset: 2, span: 8}} className="py-4">
+      <div className="h-100" style={{textAlign: 'center'}}>
+        <p className="lead">No ThreatExchange Datasets found.</p>
+        <p>
+          Datasets map to{' '}
+          <a href="https://developers.facebook.com/docs/threat-exchange/reference/apis/threat-privacy-group/v12.0">
+            Threat Privacy Groups
+          </a>{' '}
+          on ThreatExchange. Use the sync button to fetch all privacy groups you
+          have access to.
+        </p>
+      </div>
+    </Col>
+  );
+}
 
 type Dataset = {
   privacy_group_id: string;
@@ -120,17 +143,21 @@ export default function ThreatExchangeSettingsTab(): JSX.Element {
     refreshDatasets();
   }, []);
   return (
-    <>
-      <Card.Header>
-        <Row className="mt-3">
-          <h2 className="mt-2">ThreatExchange Datasets </h2>
+    <SettingsTabPane>
+      <Row className="mt-3">
+        <Col xs={{span: 8}}>
+          <h3>ThreatExchange Datasets </h3>
+        </Col>
+        <Col xs={{span: 4}} className="text-right">
           <OverlayTrigger
             key="syncButton"
-            placement="right"
+            placement="left"
             overlay={
-              <Tooltip id="tooltip-right">
-                Read PrivacyGroup metatdata from ThreatExchange to build new
-                Datasets
+              <Tooltip id="sync-info" className="p-2">
+                <p style={{textAlign: 'left'}}>
+                  Get information on all PrivacyGroups that you have access to.
+                  These will be used to fetch datasets of hashes.
+                </p>
               </Tooltip>
             }>
             <Button
@@ -152,47 +179,76 @@ export default function ThreatExchangeSettingsTab(): JSX.Element {
               Sync
             </Button>
           </OverlayTrigger>
-        </Row>
-      </Card.Header>
-      <Card.Body>
-        <Row className="mt-3">
-          <Spinner hidden={!loading} animation="border" role="status">
-            <span className="sr-only">Loading...</span>
-          </Spinner>
-          {!datasets || datasets.length === 0
-            ? null
-            : datasets.map(dataset => (
-                <ThreatExchangePrivacyGroupCard
-                  key={dataset.privacy_group_id}
-                  privacyGroupName={dataset.privacy_group_name}
-                  description={dataset.description}
-                  fetcherActive={dataset.fetcher_active}
-                  matcherActive={dataset.matcher_active}
-                  inUse={dataset.in_use}
-                  privacyGroupId={dataset.privacy_group_id}
-                  writeBack={dataset.write_back}
-                  hashCount={dataset.hash_count}
-                  matchCount={dataset.match_count}
-                  pdqMatchThreshold={dataset.pdq_match_threshold}
-                  onSave={onPrivacyGroupSave}
-                  onDelete={onPrivacyGroupDelete}
-                />
-              ))}
-        </Row>
-      </Card.Body>
-      <Col className="mx-1" md="6">
-        <HolidaysDatasetInformationBlock
-          samplePGExists={
-            datasets
-              ? datasets.some(
-                  (ds: {privacy_group_id: string}) =>
-                    ds.privacy_group_id === SAMPLE_PG_ID,
-                )
-              : false
-          }
-          refresh={refreshDatasets}
-        />
-      </Col>
-    </>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        {loading ? (
+          <Col>
+            <Spinner animation="border" role="status">
+              <span className="sr-only">Loading...</span>
+            </Spinner>
+          </Col>
+        ) : null}
+
+        {!loading && (!datasets || datasets.length === 0) ? (
+          <EmptyState />
+        ) : (
+          datasets.map(dataset => (
+            <ThreatExchangePrivacyGroupCard
+              key={dataset.privacy_group_id}
+              privacyGroupName={dataset.privacy_group_name}
+              description={dataset.description}
+              fetcherActive={dataset.fetcher_active}
+              matcherActive={dataset.matcher_active}
+              inUse={dataset.in_use}
+              privacyGroupId={dataset.privacy_group_id}
+              writeBack={dataset.write_back}
+              hashCount={dataset.hash_count}
+              matchCount={dataset.match_count}
+              pdqMatchThreshold={dataset.pdq_match_threshold}
+              onSave={onPrivacyGroupSave}
+              onDelete={onPrivacyGroupDelete}
+            />
+          ))
+        )}
+      </Row>
+      <Row>
+        <Col>
+          <hr />
+        </Col>
+      </Row>
+
+      <Row className="mt-3">
+        <Col>
+          <h3>ThreatExchange Configuration</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={{span: 8}}>
+          <ThreatExchangeTokenEditor />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <hr />
+        </Col>
+      </Row>
+
+      <Row className="mt-3">
+        <Col className="mx-1" md="8">
+          <HolidaysDatasetInformationBlock
+            samplePGExists={
+              datasets
+                ? datasets.some(
+                    (ds: {privacy_group_id: string}) =>
+                      ds.privacy_group_id === SAMPLE_PG_ID,
+                  )
+                : false
+            }
+            refresh={refreshDatasets}
+          />
+        </Col>
+      </Row>
+    </SettingsTabPane>
   );
 }
