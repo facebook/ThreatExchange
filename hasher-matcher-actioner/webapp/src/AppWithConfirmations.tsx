d58d5846ab21145d1d24d6b882ec3d58613cbf62
Dipanjan Mukherjee <dipanjanm@fb.com>
2022-02-07 01:41:33 +0000

    Consolidate confirmations into a single component
    Confirming some action on the UI is  a pretty common workflow. I have noticed many Modals scattered through the codebase all trying the same thing.
    
    This, much like toasts introduced in #897 adds common infrastructure to do confirmations.
    
    It also ports over almost all instances of confirmations. Except ActionRules because that is turning out to be harder to extract because of other changes I am making.

---
+++ hasher-matcher-actioner/webapp/src/AppWithConfirmations.tsx
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ */
+
+import React, {useState} from 'react';
+import {Button, Modal} from 'react-bootstrap';
+
+type ConfirmationMessage = {
+  message: string;
+  ctaText: string;
+  ctaVariant: 'primary' | 'success' | 'danger'; // Figure out how to refer to bootstrap variants
+  onConfirm: () => void;
+  onCancel: () => void;
+};
+
+type AppWithConfirmationsProps = {
+  children: JSX.Element | JSX.Element[];
+};
+
+export const ConfirmationsContext = React.createContext({
+  /* eslint-disable @typescript-eslint/no-empty-function */
+  confirm: ({
+    message,
+    ctaText,
+    ctaVariant,
+    onConfirm,
+    onCancel,
+  }: ConfirmationMessage) => {},
+  /* eslint-enable @typescript-eslint/no-empty-function */
+});
+
+export function AppWithConfirmations({children}: AppWithConfirmationsProps) {
+  const [confirmation, setConfirmation] = useState<
+    ConfirmationMessage | undefined
+  >(undefined);
+
+  const inAppConfirmations = {
+    confirm: ({
+      message,
+      ctaText,
+      ctaVariant,
+      onConfirm,
+      onCancel,
+    }: ConfirmationMessage) => {
+      setConfirmation({message, ctaText, ctaVariant, onConfirm, onCancel});
+    },
+  };
+
+  const handleConfirm = () => {
+    setConfirmation(undefined);
+    confirmation?.onConfirm();
+  };
+
+  const handleCancel = () => {
+    setConfirmation(undefined);
+    confirmation?.onCancel();
+  };
+
+  return (
+    <ConfirmationsContext.Provider value={inAppConfirmations}>
+      {children}
+      <Modal show={!!confirmation}>
+        <Modal.Body>{confirmation?.message}</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleConfirm} variant={confirmation?.ctaVariant}>
+            {confirmation?.ctaText}
+          </Button>
+          <Button onClick={handleCancel} variant="light">
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </ConfirmationsContext.Provider>
+  );
+}
