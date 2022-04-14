7b4a84c4526ea0473f96c1eebdd0b6b7cc874795
samyak <samyak.mt@gmail.com>
2022-04-14 19:05:11 +0000

    Remove comments and rename dataclass

--- hasher-matcher-actioner/hmalib/lambdas/api/lcc.py
+++ hasher-matcher-actioner/hmalib/lambdas/api/lcc.py
@@ -15,14 +15,8 @@ from hmalib.lambdas.api.middleware import (
 import typing as t
 
 
-# I return this type because I saw in the documentation that a
-# to_json method is required
-
-# I might need to use
-# `jsoninator(<requestObj>, from_query=True)` but not sure how the from_query=True
-# would modify this implementation
 @dataclass
-class LccResponse(JSONifiable):
+class LCCResponse(JSONifiable):
     found_match: bool
     content_id: t.Optional[str]
     preview_url: t.Optional[str]
@@ -34,32 +28,13 @@ class LccResponse(JSONifiable):
             "preview_url": self.preview_url,
         }
 
-    # def to_json(self) -> t.Dict:
-    #     return asdict(self)
 
-
-def get_lcc_api(
-    datastore_table: Table,
-    hma_config_table: str,
-    indexes_bucket_name: str,
-    writeback_queue_url: str,
-    bank_table: Table,
-    hash_queue_url: str,
-) -> bottle.Bottle:
+def get_lcc_api() -> bottle.Bottle:
 
     lcc_api = SubApp()
 
-    # Not sure if I need these two?
-    HMAConfig.initialize(hma_config_table)
-    banks_table = BanksTable(table=bank_table)
-
     @lcc_api.get("/", apply=[jsoninator])
-    def lcc_hasher() -> LccResponse:
-        # Do i need a case if these are blank?
-        # They seem to default to "" if left out in the api request
-        found_match = bottle.request.query.found_match
-        content_id = bottle.request.query.content_id
-        preview_url = bottle.request.query.preview_url
-        return LccResponse(found_match, content_id, preview_url)
+    def lcc_hasher() -> LCCResponse:
+        return LCCResponse("", "", "")
 
     return lcc_api
