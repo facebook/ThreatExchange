a6194e3e6aaa1cb55706ae5e1dae1dbc2334de75
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2022-04-19 21:29:15 +0000

    add get_index() connect lcc_hasher() to LCCIndexer

--- hasher-matcher-actioner/hmalib/lambdas/api/lcc.py
+++ hasher-matcher-actioner/hmalib/lambdas/api/lcc.py
@@ -4,6 +4,7 @@ from dataclasses import asdict, dataclass
 import bottle
 from hmalib.common.config import HMAConfig
 from hmalib.common.models.bank import BankMember, BanksTable
+from hmalib.indexers.lcc import LCCIndexer
 from hmalib.lambdas.api.middleware import DictParseable, JSONifiable, SubApp, jsoninator
 from mypy_boto3_dynamodb.service_resource import Table
 
@@ -21,14 +22,22 @@ class LCCResponse(JSONifiable):
             "preview_url": self.preview_url,
         }
 
-
-def get_lcc_api() -> bottle.Bottle:
+@functools.lru_cache(maxsize=None)
+def get_index(storage_path, signal_type):
+    index = LCCIndexer.get_recent_index(storage_path, signal_type)
+    return index
+def get_lcc_api(storage_path) -> bottle.Bottle:
 
     lcc_api = SubApp()
 
     @lcc_api.get("/", apply=[jsoninator])
     def lcc_hasher() -> LCCResponse:
-        print("Hash: ", bottle.request.query.hash)
-        return LCCResponse(False, "", "")
+        index = get_index(storage_path, bottle.request.query.hash.signal_type)
+        hash_value = bottle.request.query.hash
+        match_array = index.query(hash_value)
+        match_value = False
+        if len(match_array) != 0:
+            match_value = True
+        return LCCResponse(match_value, hash_value, match_value[0].metadata)
 
     return lcc_api
