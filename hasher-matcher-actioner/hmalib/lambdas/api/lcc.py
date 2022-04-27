7be540cb5d83820e1a1394bf4bd84806031edb09
franklin-phan <54521266+franklin-phan@users.noreply.github.com>
2022-04-27 14:20:43 -0500

    Add get_index() and  connect lcc_hasher() to LCCIndexer (#999)
    * add get_index() connect lcc_hasher() to LCCIndexer
    
    * ran black and isort
    
    * add functools import
    
    * fix: undefined envvar in api_root, explicit path for lcc API
    
    Co-authored-by: Dipanjan Mukherjee <dipanjanm@fb.com>

--- hasher-matcher-actioner/hmalib/lambdas/api/lcc.py
+++ hasher-matcher-actioner/hmalib/lambdas/api/lcc.py
@@ -1,15 +1,20 @@
+import functools
 import typing as t
 from dataclasses import asdict, dataclass
-
 import bottle
-from hmalib.common.config import HMAConfig
-from hmalib.common.models.bank import BankMember, BanksTable
-from hmalib.lambdas.api.middleware import DictParseable, JSONifiable, SubApp, jsoninator
+
 from mypy_boto3_dynamodb.service_resource import Table
+
+from threatexchange.signal_type.index import SignalTypeIndex
+from threatexchange.signal_type.signal_base import SignalType
+
+from hmalib.common.mappings import HMASignalTypeMapping
+from hmalib.indexers.lcc import LCCIndexer
+from hmalib.lambdas.api.middleware import DictParseable, JSONifiable, SubApp, jsoninator
 
 
 @dataclass
-class LCCResponse(JSONifiable):
+class RecentlySeenLCCResponse(JSONifiable):
     found_match: bool
     content_id: t.Optional[str]
     preview_url: t.Optional[str]
@@ -22,13 +27,48 @@ class LCCResponse(JSONifiable):
         }
 
 
-def get_lcc_api() -> bottle.Bottle:
+@functools.lru_cache(maxsize=None)
+def get_index(storage_path: str, signal_type_name: str):
+    index = LCCIndexer.get_recent_index(storage_path, signal_type_name)
+    return index
+
+
+def get_lcc_api(
+    signal_type_mapping: HMASignalTypeMapping, storage_path: str
+) -> bottle.Bottle:
+    """
+    Some APIs to provide live content clustering cabilities. This will index all
+    recently seen content and provide a way to query for content_ids matching a
+    given hash.
+    """
 
     lcc_api = SubApp()
 
-    @lcc_api.get("/", apply=[jsoninator])
-    def lcc_hasher() -> LCCResponse:
-        print("Hash: ", bottle.request.query.hash)
-        return LCCResponse(False, "", "")
+    @lcc_api.get("/recently-seen/", apply=[jsoninator])
+    def recently_seen_in_lcc() -> RecentlySeenLCCResponse:
+        """
+        Given a signal_type and a hash, have we seen something like this
+        recently? Uses default precision knobs.
+
+        TODO: Make thresholds configurable.
+        """
+        signal_type = signal_type_mapping.get_signal_type_enforce(
+            bottle.request.query.signal_type
+        )
+        hash_value = bottle.request.query.hash
+
+        index = get_index(storage_path, signal_type.get_name())
+
+        match_array = index.query(hash_value)
+        found_match = bool(len(match_array))
+
+        if not found_match:
+            return RecentlySeenLCCResponse(False, None, None)
+
+        # Use the first result as the content_id, TODO: convert content_Id to
+        # preview_url, also add content_type
+        content_id = match_array[0].metadata
+        preview_url = content_id
+        return RecentlySeenLCCResponse(found_match, content_id, preview_url)
 
     return lcc_api
