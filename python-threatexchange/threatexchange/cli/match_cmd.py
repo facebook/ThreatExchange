a318dd1b1bd25f7c57fe34f7b12a045b00be3d15
David Callies <dcallies@fb.com>
2022-06-20 19:26:24 -0400

    [pytx] Make hash input same as match

--- python-threatexchange/threatexchange/cli/match_cmd.py
+++ python-threatexchange/threatexchange/cli/match_cmd.py
@@ -57,7 +57,7 @@ class MatchCommand(command_base.Command):
     USE_STDIN = "-"
 
     @classmethod
-    def init_argparse(cls, settings: CLISettings, ap) -> None:
+    def init_argparse(cls, settings: CLISettings, ap: argparse.ArgumentParser) -> None:
 
         ap.add_argument(
             "content_type",
