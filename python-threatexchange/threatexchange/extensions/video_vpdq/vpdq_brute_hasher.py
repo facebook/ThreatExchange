136b18f73b9f6c738e4ca7dfa583a10f77464139
Xiangnan Chen <xiangnan@fb.com>
2022-07-13 14:23:46 -0400

    Modify example

--- python-threatexchange/threatexchange/extensions/video_vpdq/vpdq_brute_hasher.py
+++ python-threatexchange/threatexchange/extensions/video_vpdq/vpdq_brute_hasher.py
@@ -38,7 +38,7 @@ def match_VPDQ_hash_brute(
     query_hash: t.List[vpdq.VpdqFeature],
     quality_tolerance: int,
     distance_tolerance: int,
-) -> t.NamedTuple:
+) -> VPDQMatchResult:
     """Match two VPDQ hashes. Return the query-match percentage and target-match percentage
 
     Args:
