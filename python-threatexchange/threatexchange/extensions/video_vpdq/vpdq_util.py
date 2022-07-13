ddef2f5533371b863c47396fc530c3b4183f40fb
Xiangnan Chen <xiangnan@fb.com>
2022-07-13 15:23:55 -0400

    Update naming and docstring

--- python-threatexchange/threatexchange/extensions/video_vpdq/vpdq_util.py
+++ python-threatexchange/threatexchange/extensions/video_vpdq/vpdq_util.py
@@ -9,7 +9,7 @@ from dataclasses import dataclass
 QUALITY = "quality"
 HASH = "hash"
 TIMESTAMP = "timestamp"
-VPDQ_FEATURE_NUM = 4
+VPDQ_STR_FEATURE_NUM = 4
 
 
 @dataclass
@@ -50,61 +50,71 @@ def json_to_vpdq(json_str: str) -> t.List[vpdq.VpdqFeature]:
     return features
 
 
-def dedupe(hashes: t.List[vpdq.VpdqFeature]) -> t.List[vpdq.VpdqFeature]:
+def dedupe(features: t.List[vpdq.VpdqFeature]) -> t.List[vpdq.VpdqFeature]:
     """Filter out the VPDQ feature with exact same hash in a list of VPDQ features
 
     Args:
-        hashes
+        features
 
     Returns:
-        list of VPDQ feature: List of VPDQeatures with unique hashes
+        list of VPDQ feature: List of VPDQeatures with unique features
     """
-    unique_hashes = set()
+    unique_features = set()
     ret = []
-    for h in hashes:
-        if h.hex not in unique_hashes:
+    for h in features:
+        if h.hex not in unique_features:
             ret.append(h)
-            unique_hashes.add(h.hex)
+            unique_features.add(h.hex)
     return ret
 
 
 def quality_filter(
-    hashes: t.List[vpdq.VpdqFeature], quality_tolerance: int
+    features: t.List[vpdq.VpdqFeature], quality_tolerance: int
 ) -> t.List[vpdq.VpdqFeature]:
     """Filter VPDQ feature that has a quality lower than quality_tolerance
 
     Args:
-        hashes
+        features
         distance_tolerance : If frames is this quality level then it will be ignored
 
     Returns:
         List of VPDQFeatures with quality higher than distance_tolerance
     """
-    return list(filter(lambda hash: hash.quality >= quality_tolerance, hashes))
+    return list(filter(lambda hash: hash.quality >= quality_tolerance, features))
 
 
 def read_file_to_hash(
     input_hash_filename: t.Union[str, pathlib.Path]
 ) -> t.List[vpdq.VpdqFeature]:
-    """Read hash file and return vpdq hash
+    """Read hash file and return list of VPDQ features
 
     Args:
         input_hash_filename : Input hash file path
 
     Returns:
-        vpdq hash from the hash file"""
+        VPDQ features from the hash file"""
 
-    hash = []
+    features = []
     with open(input_hash_filename, "r") as file:
         for line in file.readlines():
             line = line.strip()
-            content = line.split(",")
-            if len(content) != VPDQ_FEATURE_NUM:
-                raise ValueError("Reading invalid VPDQ hash file")
-            pdq_hash = vpdq.str_to_hash(content[2])
-            feature = vpdq.VpdqFeature(
-                int(content[1]), int(content[0]), pdq_hash, float(content[3])
-            )
-            hash.append(feature)
+            features.append(str_to_feature(line))
+    return features
 
-    return hash
+
+def str_to_feature(hash_str: str) -> vpdq.VpdqFeature:
+    """Convert string to a VPDQ feature
+
+    Args:
+        hash_str : VPDQ feature's string representation
+        frame_number,quality,hash(hex_str),timestamp
+    Returns:
+        VPDQ feature from the string"""
+    content = hash_str.split(",")
+    if len(content) != VPDQ_STR_FEATURE_NUM:
+        raise ValueError("Reading invalid VPDQ hash file")
+    vpdq_hash = vpdq.str_to_hash(content[2])
+    feature = vpdq.VpdqFeature(
+        int(content[1]), int(content[0]), vpdq_hash, float(content[3])
+    )
+    return feature
