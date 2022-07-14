045ced93cf4fb9593fe61f414206985000170ccf
Xiangnan Chen <xiangnan@fb.com>
2022-07-14 13:46:39 -0400

    Update test to check percentage

--- python-threatexchange/threatexchange/extensions/video_vpdq/tests/test_vpdq_hash_and_match.py
+++ python-threatexchange/threatexchange/extensions/video_vpdq/tests/test_vpdq_hash_and_match.py
@@ -13,7 +13,11 @@ else:
     from threatexchange.extensions.video_vpdq.video_vpdq import VideoVPDQSignal
     from threatexchange.extensions.video_vpdq.vpdq_util import (
         vpdq_to_json,
+        json_to_vpdq,
         read_file_to_hash,
+    )
+    from threatexchange.extensions.video_vpdq.vpdq_brute_hasher import (
+        match_VPDQ_hash_brute,
     )
 
 VIDEO = "tmk/sample-videos/chair-20-sd-bar.mp4"
@@ -26,8 +30,12 @@ class TestVPDQHasherMatcher:
     def test_vpdq_from_string_path(self):
         computed_hash = VideoVPDQSignal.hash_from_file(ROOTDIR / VIDEO)
         expected_hash = read_file_to_hash(ROOTDIR / HASH)
-        res, _ = VideoVPDQSignal.compare_hash(
-            computed_hash, vpdq_to_json(expected_hash)
+        assert vpdq_to_json(json_to_vpdq(computed_hash)) == computed_hash
+        res = match_VPDQ_hash_brute(
+            json_to_vpdq(computed_hash),
+            expected_hash,
+            VideoVPDQSignal.VPDQ_CONFIDENT_QUALITY_THRESHOLD,
+            VideoVPDQSignal.VPDQ_CONFIDENT_DISTANCE_THRESHOLD,
         )
-        assert res.target_match_percent == 100
         assert res.query_match_percent == 100
+        assert res.target_match_percent == 100
