7119bd0eff16aefc251f6350aef8bd142211470b
Xiangnan Chen <xiangnan@fb.com>
2022-07-20 11:14:14 -0400

    refactor index and matcher functions

--- python-threatexchange/threatexchange/extensions/video_vpdq/vpdq_faiss_matcher.py
+++ python-threatexchange/threatexchange/extensions/video_vpdq/vpdq_faiss_matcher.py
@@ -2,11 +2,12 @@
 
 import vpdq
 import faiss
-from .vpdq_util import dedupe, quality_filter
 from threatexchange.extensions.video_vpdq.vpdq_util import (
     VPDQMatchResult,
     BITS_IN_VPDQ,
     VPDQ_VIDEOID_TYPE,
+    quality_filter,
+    dedupe
 )
 import typing as t
 import numpy
@@ -23,8 +24,6 @@ class VPDQFlatHashIndex:
     def __init__(self) -> None:
         faiss_index = faiss.IndexBinaryFlat(BITS_IN_VPDQ)
         self.faiss_index = faiss_index
-        self.idx_to_vpdq: t.List[t.Tuple[VPDQ_VIDEOID_TYPE, vpdq.VpdqFeature]] = []
-        self.video_id_to_vpdq: t.Dict[VPDQ_VIDEOID_TYPE, t.List[vpdq.VpdqFeature]] = {}
         super().__init__()
 
     def get_video_frame_counts(
@@ -40,52 +39,37 @@ class VPDQFlatHashIndex:
         """
         return len(quality_filter(self.video_id_to_vpdq[video_id], quality_tolerance))
 
-    def add_single_video(
-        self,
-        hashes: t.List[vpdq.VpdqFeature],
-        video_id: VPDQ_VIDEOID_TYPE,
-    ) -> None:
+    def add_single_video(self, hashes: t.List[vpdq.VpdqFeature]) -> None:
         """
         Args:
             hashes : One video's VPDQ features of to create the index with
-            video_id : Unique video id corresponds to video
         """
-        if video_id in self.video_id_to_vpdq:
-            raise ValueError("invalid VPDQ Index Video ID, this ID already exists")
-        hashes = dedupe(hashes)
-        self.idx_to_vpdq.extend([(video_id, h) for h in hashes])
-        self.video_id_to_vpdq[video_id] = hashes
         hex_hashes = [h.hex for h in hashes]
         hash_bytes = [binascii.unhexlify(h) for h in hex_hashes]
-        vectors = list(
-            map(lambda h: numpy.frombuffer(h, dtype=numpy.uint8), hash_bytes)
-        )
+        vectors = [map(lambda h: numpy.frombuffer(h, dtype=numpy.uint8), hash_bytes)]
         self.faiss_index.add(numpy.array(vectors))
 
     def search_with_raw_features_in_result(
-        self,
-        queries: t.List[vpdq.VpdqFeature],
-        quality_tolerance: int,
-        distance_tolerance: int,
+        self, queries: t.List[vpdq.VpdqFeature], distance_tolerance: int
     ) -> t.Dict[str, t.List]:
         """
-        Searches this index for PDQ hashes within the index that are no more than the threshold away from the query hashes by
-        hamming distance.
+        Searches this index for PDQ hashes within the index that are no more than the threshold away
+        from the query hashes by hamming distance.
 
         Args:
             queries : The VPDQ features to against the index
             quality_tolerance : The quality tolerance of matching two frames.
             If either frames is below this quality level then they will not be queired and added to result
-            distance_tolerance : Threshold value to use for this search. The hamming distance between the result hashes and the related query will
-            be no more than the threshold value. i.e., hamming_dist(q_i,r_i_j) <= threshold.
+            distance_tolerance : Threshold value to use for this search. The hamming distance between the result hashes
+            and the related query will be no more than the threshold value. i.e., hamming_dist(q_i,r_i_j) <= threshold.
 
         Returns:
         sequence of matches per query
             For each query provided in queries, the returned sequence will contain a sequence of matches within the index
             that were within threshold hamming distance of that query. These matches will be (id, video_id, frame_number,
-            hex_str of hash, quality, timestamp, distance). The inner sequences may be empty in the case of no hashes within the index.
-            The same VPDQ feature may also appear in more than one inner sequence if it matches multiple query hashes.
-            For example the hash "000000000000000000000000000000000000000000000000000000000000ffff" would match both
+            hex_str of hash, quality, timestamp, distance). The inner sequences may be empty in the case of no hashes
+            within the index. The same VPDQ feature may also appear in more than one inner sequence if it matches multiple
+            query hashes. For example the hash "000000000000000000000000000000000000000000000000000000000000ffff" would match both
             "00000000000000000000000000000000000000000000000000000000fffffff" and
             "0000000000000000000000000000000000000000000000000000000000000000" for a threshold of 16. Thus it would appear in
             the entry for both the hashes if they were both in the queries list.
@@ -99,7 +83,6 @@ class VPDQFlatHashIndex:
             }
         """
 
-        queries = quality_filter(dedupe(queries), quality_tolerance)
         query_vectors = [
             numpy.frombuffer(binascii.unhexlify(q.hex), dtype=numpy.uint8)
             for q in queries
@@ -115,69 +98,12 @@ class VPDQFlatHashIndex:
             matches = [idx.item() for idx in I[limits[i] : limits[i + 1]]]
             distances = [idx for idx in similarities[limits[i] : limits[i + 1]]]
             for match, distance in zip(matches, distances):
-                video_id, vpdq_match = self.idx_to_vpdq[match]
-                if vpdq_match.quality >= quality_tolerance:
-                    match_tuples.append(
-                        (
-                            match,
-                            video_id,
-                            vpdq_match.frame_number,
-                            vpdq_match.hex,
-                            vpdq_match.quality,
-                            vpdq_match.timestamp,
-                            distance,
-                        )
+                match_tuples.append(
+                    (
+                        match,
+                        distance,
                     )
+                )
             result[query.hex] = match_tuples
         return result
 
-    def search_with_match_percentage_in_result(
-        self,
-        query_hash: t.List[vpdq.VpdqFeature],
-        quality_tolerance: int,
-        distance_tolerance: int,
-    ) -> t.List[t.Tuple[VPDQ_VIDEOID_TYPE, VPDQMatchResult]]:
-        """Searches this VPDQ index for query hashes within the index that are no more than the threshold away from the query hashes by
-            hamming distance.
-
-        Args:
-            query_hash : Query VPDQ hash
-            VPDQ_index : VPDQ index to be searched for query hash
-            quality_tolerance : The quality tolerance of matching two frames.
-            If either frames is below this quality level then they will not be queired and added to result
-            distance_tolerance : The hamming distance tolerance of between two frames.
-            If the hamming distance is bigger than the tolerance, it will be considered as unmatched
-
-        Returns:
-            VPDQ Video id corresponds with its VPDQMatchResult
-        """
-        ret = self.search_with_raw_features_in_result(
-            query_hash, quality_tolerance, distance_tolerance
-        )
-        query_matched: t.Dict[VPDQ_VIDEOID_TYPE, t.Set] = {}
-        index_matched: t.Dict[VPDQ_VIDEOID_TYPE, t.Set] = {}
-        for r in ret:
-            for matched_frame in ret[r]:
-                # query_str =>  (id, video_id, frame_number, hex_str of hash, quality, timestamp, distance)
-                _, video_id, frame_number, _, quality, _, _ = matched_frame
-
-                if video_id not in query_matched:
-                    query_matched[video_id] = set()
-                query_matched[video_id].add(r)
-
-                if video_id not in index_matched:
-                    index_matched[video_id] = set()
-                index_matched[video_id].add(frame_number)
-
-        return [
-            (
-                video_id,
-                VPDQMatchResult(
-                    len(query_matched[video_id]) * 100 / len(query_hash),
-                    len(index_matched[video_id])
-                    * 100
-                    / self.get_video_frame_counts(video_id, quality_tolerance),
-                ),
-            )
-            for video_id in sorted(query_matched)
-        ]
--- python-threatexchange/threatexchange/extensions/video_vpdq/vpdq_index.py
+++ python-threatexchange/threatexchange/extensions/video_vpdq/vpdq_index.py
@@ -6,18 +6,16 @@ hashing.pdq_faiss_matcher.
 """
 
 import typing as t
-
+import vpdq
 from threatexchange.signal_type.index import (
     SignalTypeIndex,
     IndexMatch,
     T as IndexT,
 )
-
 from threatexchange.extensions.video_vpdq.vpdq_faiss_matcher import VPDQFlatHashIndex
-from threatexchange.extensions.video_vpdq.vpdq_util import json_to_vpdq
+from threatexchange.extensions.video_vpdq.vpdq_util import json_to_vpdq, dedupe, quality_filter, vpdq_to_json,VPDQMatchResult
 from threatexchange.extensions.video_vpdq.video_vpdq import VideoVPDQSignal
 
-VIDEO_ID = "video_id"
 
 
 class VPDQFlatIndex(SignalTypeIndex):
@@ -37,10 +35,11 @@ class VPDQFlatIndex(SignalTypeIndex):
     def _get_empty_index(cls) -> VPDQFlatHashIndex:
         return VPDQFlatHashIndex()
 
-    def __init__(self, entries: t.Iterable[t.Tuple[str, t.Dict]] = ()) -> None:
+    def __init__(self, entries: t.Iterable[t.Tuple[str, IndexT]] = ()) -> None:
         super().__init__()
         self.index: VPDQFlatHashIndex = self._get_empty_index()
-        self.video_id_to_entry: t.Dict = {}
+        self.entries: t.List = []
+        self.idx_to_vpdq: t.List[t.Tuple[int, vpdq.VpdqFeature]] = []
         self.add_all(entries=entries)
 
     def query(self, hash: str) -> t.List[IndexMatch[IndexT]]:
@@ -49,12 +48,7 @@ class VPDQFlatIndex(SignalTypeIndex):
         """
 
         # query takes a signal hash but index supports batch queries hence [hash]
-        features = json_to_vpdq(hash)
-        results = self.index.search_with_match_percentage_in_result(
-            features,
-            VideoVPDQSignal.VPDQ_CONFIDENT_QUALITY_THRESHOLD,
-            VideoVPDQSignal.VPDQ_CONFIDENT_DISTANCE_THRESHOLD,
-        )
+        results = self.query_raw_result(hash)
         matches = []
         for match in results:
             video_id, match_result = match
@@ -70,18 +64,87 @@ class VPDQFlatIndex(SignalTypeIndex):
         features = json_to_vpdq(hash)
         results = self.index.search_with_raw_features_in_result(
             features,
-            VideoVPDQSignal.VPDQ_CONFIDENT_QUALITY_THRESHOLD,
             VideoVPDQSignal.VPDQ_CONFIDENT_DISTANCE_THRESHOLD,
         )
-        return results
+        matches = {}
+        for hash in results:
+            match_tuples = []
+            for match in hash:
+                entry_id, vpdq_match = self.idx_to_vpdq[match[0]]
+                match_tuples.append([vpdq_match, match[1], self.entries[entry_id]])
+            matches[hash] = match_tuples
+        return matches
 
     def add(self, signal_str: str, entry: t.Dict) -> None:
-        if entry[VIDEO_ID] is None:
-            raise ValueError("invalid VPDQ entry, this must exist a video_id")
-        video_id = entry[VIDEO_ID]
-        self.video_id_to_entry[video_id] = entry
-        self.index.add_single_video(json_to_vpdq(signal_str), video_id)
+        entry_id = len(self.entries)
+        hashes = dedupe(json_to_vpdq(signal_str))
+        self.entries.append(entry)
+        self.idx_to_vpdq.extend([(entry_id, h) for h in hashes])
+        self.index.add_single_video(json_to_vpdq(signal_str))
 
     def add_all(self, entries: t.Iterable[t.Tuple[str, t.Dict]]) -> None:
         for signal_str, entry in entries:
             self.add(signal_str, entry)
+
+    def get_video_frame_counts(self, idx: int) -> int:
+        """
+        Args:
+            idx : Unique id corresponds to video
+            quality_tolerance : The quality tolerance of frames.
+            If frame is below this quality level then they will not be counted
+        Returns:
+            Size of VPDQ features in the video that has a quality larger or equal to quality_tolerance
+        """
+        return len(self.idx_to_entries[idx])
+
+    def query_with_match_percentage_in_result(
+        self,
+        query_hash: t.List[vpdq.VpdqFeature],
+    ) -> t.List[t.Tuple[int, VPDQMatchResult]]:
+        """Searches this VPDQ index for query hashes within the index that are no more than the threshold away
+        from the query hashes by hamming distance.
+
+        Args:
+            query_hash : Query VPDQ hash
+            VPDQ_index : VPDQ index to be searched for query hash
+            quality_tolerance : The quality tolerance of matching two frames.
+            If either frames is below this quality level then they will not be queired and added to result
+            distance_tolerance : The hamming distance tolerance of between two frames.
+            If the hamming distance is bigger than the tolerance, it will be considered as unmatched
+
+        Returns:
+            VPDQ Video id corresponds with its VPDQMatchResult
+        """
+        features = json_to_vpdq(hash)
+        results = self.index.search_with_raw_features_in_result(
+            features,
+            VideoVPDQSignal.VPDQ_CONFIDENT_DISTANCE_THRESHOLD,
+        )
+        query_matched: t.Dict[int, t.Set] = {}
+        index_matched: t.Dict[int, t.Set] = {}
+        for hash in results:
+            for match in hash:
+                # query_str =>  (match, entry)
+                entry_id, vpdq_match = self.idx_to_vpdq[match[0]]
+
+                if entry_id not in query_matched:
+                    query_matched[entry_id] = set()
+                query_matched[entry_id].add(hash)
+
+                if entry_id not in index_matched:
+                    index_matched[entry_id] = set()
+                index_matched[entry_id].add(vpdq_match.frame_number)
+
+        return [
+            (
+                video_id,
+                VPDQMatchResult(
+                    len(query_matched[video_id]) * 100 / len(query_hash),
+                    len(index_matched[video_id])
+                    * 100
+                    / self.get_video_frame_counts(video_id),
+                ),
+            )
+            for video_id in sorted(query_matched)
+        ]
+
