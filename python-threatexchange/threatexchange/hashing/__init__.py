7b7cbd6edd065bbb0523426f8351f8751a8457b0
Barrett Olson <barrett@fb.com>
2022-07-22 10:55:35 -0400

    faiss is now a required lib for pytx so we dont not need this check

--- python-threatexchange/threatexchange/hashing/__init__.py
+++ python-threatexchange/threatexchange/hashing/__init__.py
@@ -1,16 +1,1 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-import warnings
-
-from threatexchange.hashing.pdq_utils import BITS_IN_PDQ
-
-try:
-    from threatexchange.hashing.pdq_faiss_matcher import (
-        PDQHashIndex,
-        PDQFlatHashIndex,
-        PDQMultiHashIndex,
-    )
-except:
-    warnings.warn(
-        "pdq matchers require faiss to be installed; install threatexchange with the [faiss] extra to use them",
-        category=ImportWarning,
-    )
