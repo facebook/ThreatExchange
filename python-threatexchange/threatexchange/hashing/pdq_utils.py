e83b979ee422588f44c4549dfb23d74c6394d130
Xiangnan Chen <xiangnan@fb.com>
2022-07-25 09:14:01 -0400

    Add comments

--- python-threatexchange/threatexchange/hashing/pdq_utils.py
+++ python-threatexchange/threatexchange/hashing/pdq_utils.py
@@ -53,17 +53,17 @@ def pdq_match(pdq_hex_a: str, pdq_hex_b: str, threshold: int) -> bool:
 
 
 def get_zero_hash() -> str:
-    """Return a pdq hash str that is zero for every byte"""
+    """Return a pdq hash hex str that is zero for every byte"""
     return "0" * PDQ_HEX_STR_LEN
 
 
 def get_random_hash() -> str:
-    """Return a random pdq hash str"""
+    """Return a random pdq hash hex str"""
     return f"{random.randrange(2**256):0{PDQ_HEX_STR_LEN}x}"
 
 
 def get_similar_hash(pdq_hex: str, dist: int) -> str:
-    """Return a pdq hash with dist hamming distance away from pdq_hex"""
+    """Return a pdq hash hex str with dist hamming distance away from pdq_hex"""
     if dist > BITS_IN_PDQ or dist < 0:
         raise ValueError("Invalid distance value")
     order = random.sample(range(BITS_IN_PDQ), k=dist)
