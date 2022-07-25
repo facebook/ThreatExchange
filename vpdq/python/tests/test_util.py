47a045fee0e76b6e9ca7255e9c03551d21884a1c
Xiangnan Chen <xiangnan@fb.com>
2022-07-25 09:51:38 -0400

    Add more tests for vpdq package

--- vpdq/python/tests/test_util.py
+++ vpdq/python/tests/test_util.py
@@ -1,8 +1,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import vpdq
+import typing as t
 
 
-def read_file_to_hash(input_hash_filename: str) -> list[vpdq.VpdqFeature]:
+def read_file_to_hash(input_hash_filename: str) -> t.List[vpdq.VpdqFeature]:
     """Read hash file and return vpdq hash
 
     Args:
@@ -33,5 +34,5 @@ def output_hash_to_file(output_hash_filename, hashes):
 
     with open(output_hash_filename, "w") as file:
         for cur in hashes:
-            file.write(f"{cur.frame_number},{cur.quality},{hash_to_hex(cur.hash['w'])},{cur.timestamp:.3f}")
+            file.write(f"{cur.frame_number},{cur.quality},{vpdq.hash_to_hex(cur.hash['w'])},{cur.timestamp:.3f}")
             file.write("\n")
