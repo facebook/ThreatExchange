332ab55f160d76b729008c53a2e6938b2ab1bb18
Xiangnan Chen <xiangnan@fb.com>
2022-08-01 08:50:12 -0400

    Reformat

--- python-threatexchange/threatexchange/extensions/vpdq/__init__.py
+++ python-threatexchange/threatexchange/extensions/vpdq/__init__.py
@@ -1,11 +1,13 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import sys
-if hasattr(sys, '_called_from_test'):
+
+if hasattr(sys, "_called_from_test"):
     # called from within a py test run
     pass
 else:
     from threatexchange.extensions.manifest import ThreatExchangeExtensionManifest
     from threatexchange.extensions.vpdq.video_vpdq import VideoVPDQSignal
+
     TX_MANIFEST = ThreatExchangeExtensionManifest(
         signal_types=(VideoVPDQSignal,),
     )
