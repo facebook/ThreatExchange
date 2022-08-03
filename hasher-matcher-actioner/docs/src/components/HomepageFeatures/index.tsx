02611d87fe0670b5280122d71fdce913040b186a
Barrett Olson <barrett@fb.com>
2022-08-03 17:54:02 +0000

    remove placeholder text in footer of doc landing page

--- hasher-matcher-actioner/docs/src/components/HomepageFeatures/index.tsx
+++ hasher-matcher-actioner/docs/src/components/HomepageFeatures/index.tsx
@@ -8,33 +8,7 @@ type FeatureItem = {
 };
 
 const FeatureList: FeatureItem[] = [
-  {
-    title: 'Easy to Use',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+
 ];
 
 function Feature({title, description}: FeatureItem) {
