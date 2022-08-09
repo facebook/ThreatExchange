b7f718ad0ca34bb7a8924cd2393151e1c41b4392
Dipanjan Mukherjee <dipanjanm@fb.com>
2022-08-09 16:04:16 +0000

    Allow import of threatexchange.signal_type.pdq.PdqSignal
    Summary
    ---
    This change makes tx.signal_type.pdq consistent with other modules like
    tx.signal_type.md5 and tx.signal_type.raw_text.
    
    pdq is the only module that is also a subpackage,
    
    Test
    ---
    CI.

--- python-threatexchange/threatexchange/signal_type/pdq/__init__.py
+++ python-threatexchange/threatexchange/signal_type/pdq/__init__.py
@@ -1,1 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+# Allow import of threatexchange.signal_type.pdq.PdqSignal consistent with other
+# signal types.
+from .signal import PdqSignal
