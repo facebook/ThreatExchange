04c25a5f2cc0b38de56da3be09bf131ff9d73df0
Dipanjan Mukherjee <dipanjanm@fb.com>
2022-12-01 23:11:49 +0000

    [smol] Get docker build to work and miscellaneous changes
    Summary
    ---
    - Docker copy does not succeed without atleast one file
    - add python-threatexchange to our regular devcontainer workspaces so we've got python-threatexchange to work with
    - add working directory to migrations; it was failing on our CI deploy
    - update keyserver key in devcontainer dockerfile - the current one stopped working and stackoverflow gave this answer
    
    Test Plan
    ---
    Will know after CI. Locally making these changes on our ec2 host allowed CI to work.

--- hasher-matcher-actioner/terraform/migrations/main.tf
+++ hasher-matcher-actioner/terraform/migrations/main.tf
@@ -7,12 +7,14 @@
 # AFTER v0.1.2 https://github.com/facebook/ThreatExchange/releases/tag/HMA-v0.1.2
 resource "null_resource" "default_signal_content_type_configs" {
   provisioner "local-exec" {
-    command = "python -m hmalib.scripts.cli.main migrate 2022_04_02_default_content_signal_type_configs --config-table ${var.config_table.name}"
+    working_dir = "../../"
+    command     = "python -m hmalib.scripts.cli.main migrate 2022_04_02_default_content_signal_type_configs --config-table ${var.config_table.name}"
   }
 }
 
 resource "null_resource" "default_signal_exchange_apis" {
   provisioner "local-exec" {
-    command = "python -m hmalib.scripts.cli.main migrate 2022_07_24_default_signal_exchange_apis --config-table ${var.config_table.name}"
+    working_dir = "../../"
+    command     = "python -m hmalib.scripts.cli.main migrate 2022_07_24_default_signal_exchange_apis --config-table ${var.config_table.name}"
   }
 }
