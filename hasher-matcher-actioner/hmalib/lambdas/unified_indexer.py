d4f51f8f4690d4398e906231e82f12a5a950c7db
Dipanjan Mukherjee <dipanjanm@fb.com>
2022-12-06 02:24:48 +0000

    missed out unified_indexer.py

--- hasher-matcher-actioner/hmalib/lambdas/unified_indexer.py
+++ hasher-matcher-actioner/hmalib/lambdas/unified_indexer.py
@@ -8,23 +8,19 @@ import boto3
 from threatexchange.signal_type.md5 import VideoMD5Signal
 from threatexchange.signal_type.pdq import PdqSignal
 from threatexchange.signal_type.signal_base import SignalType
+from threatexchange.signal_type.index import SignalTypeIndex
 
 from hmalib.common.config import HMAConfig
-from hmalib.common.mappings import get_pytx_functionality_mapping
 from hmalib.common.models.bank import BanksTable
 from hmalib.common.s3_adapters import (
     HashRowT,
-    ThreatExchangeS3Adapter,
-    ThreatExchangeS3PDQAdapter,
-    ThreatExchangeS3VideoMD5Adapter,
     S3ThreatDataConfig,
 )
 from hmalib.common.logging import get_logger
 from hmalib import metrics
 from hmalib.indexers.metadata import BankedSignalIndexMetadata
-from hmalib.indexers.s3_indexers import (
-    S3BackedInstrumentedIndexMixin,
-)
+from hmalib.indexers.index_store import S3PickledIndexStore
+from hmalib.lambdas.common import get_signal_type_mapping
 
 logger = get_logger(__name__)
 dynamodb = boto3.resource("dynamodb")
@@ -95,15 +91,6 @@ def merge_hash_rows_on_hash_value(
     return accumulator
 
 
-# Maps from signal type to the subclass of ThreatExchangeS3Adapter.
-# ThreatExchangeS3Adapter is used to fetch all the data corresponding to a
-# signal_type. At some point, we must allow _updates_ to indexes rather than
-# rebuilding them all the time.
-_ADAPTER_MAPPING: t.Dict[t.Type[SignalType], t.Type[ThreatExchangeS3Adapter]] = {
-    PdqSignal: ThreatExchangeS3PDQAdapter,
-    VideoMD5Signal: ThreatExchangeS3VideoMD5Adapter,
-}
-
 # Which signal types must be processed into an index?
 ALL_INDEXABLE_SIGNAL_TYPES = [PdqSignal, VideoMD5Signal]
 
@@ -121,55 +108,30 @@ def lambda_handler(event, context):
     - the key name must return a signal_type in
       ThreatUpdateS3Store.get_signal_type_from_object_key
     """
-    # Note: even though we know which files were updated, threatexchange indexes
-    # do not yet allow adding new entries. So, we must do a full rebuild. So, we
-    # only end up using the signal types that were updated, not the actual files
-    # that changed.
-
-    s3_config = S3ThreatDataConfig(
-        threat_exchange_data_bucket_name=THREAT_EXCHANGE_DATA_BUCKET_NAME,
-        threat_exchange_data_folder=THREAT_EXCHANGE_DATA_FOLDER,
-    )
-
-    banks_table = BanksTable(dynamodb.Table(BANKS_TABLE))
 
     HMAConfig.initialize(HMA_CONFIG_TABLE)
-    signal_content_mapping = get_pytx_functionality_mapping()
+    signal_content_mapping = get_signal_type_mapping()
+
+    index_store = S3PickledIndexStore(INDEXES_BUCKET_NAME)
+    banks_table = BanksTable(dynamodb.Table(BANKS_TABLE), signal_content_mapping)
 
     for signal_type in ALL_INDEXABLE_SIGNAL_TYPES:
-        adapter_class = _ADAPTER_MAPPING[signal_type]
-        data_files = adapter_class(
-            config=s3_config, metrics_logger=metrics.names.indexer
-        ).load_data()
-
         with metrics.timer(metrics.names.indexer.get_bank_data):
             bank_data = get_all_bank_hash_rows(signal_type, banks_table)
 
         with metrics.timer(metrics.names.indexer.merge_datafiles):
             logger.info(f"Merging {signal_type} Hash files")
 
-            # go from dict[filename, list<hash rows>] → list<hash rows>
-            flattened_data = [
-                hash_row for file_ in data_files.values() for hash_row in file_
-            ]
-
             merged_data = functools.reduce(
-                merge_hash_rows_on_hash_value, flattened_data + bank_data, {}
+                merge_hash_rows_on_hash_value, bank_data, {}
             ).values()
 
         with metrics.timer(metrics.names.indexer.build_index):
             logger.info(f"Rebuilding {signal_type} Index")
 
-            for index_class in [
-                signal_type.get_index_cls()
-                for signal_type in signal_content_mapping.signal_and_content.signal_type_by_name.values()
-            ]:
-                index: S3BackedInstrumentedIndexMixin = index_class.build(merged_data)
-
-                logger.info(
-                    f"Putting {signal_type} index in S3 for index {index.get_index_class_name()}"
-                )
-                index.save(bucket_name=INDEXES_BUCKET_NAME)
+            index: SignalTypeIndex = signal_type.get_index_cls().build(merged_data)
+            logger.info(f"Putting {signal_type} index in S3.")
+            index_store.save(index)
             metrics.flush()
 
     logger.info("Index updates complete")
