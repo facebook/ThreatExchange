115fd550cfe89e6d48c314d32e76bfcbd9833cef
ian w <52143079+ianwal@users.noreply.github.com>
2023-07-18 18:05:54 -0700

    [vpdq] Regression test improvements for local development (#1328)

--- vpdq/cpp/regtest.py
+++ vpdq/cpp/regtest.py
@@ -1,20 +1,37 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
 import subprocess
-import os
 import sys
 import argparse
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from shutil import copyfile
+import glob
+import platform
+from typing import Union
+
+DIR = Path(__file__).parent
+VPDQ_DIR = DIR.parent
+SAMPLE_HASHES_DIR = VPDQ_DIR / "sample-hashes"
+EXEC_DIR = VPDQ_DIR / "cpp/build"
+
+
+def get_os() -> str:
+    if platform.system() == "Windows":
+        return "Windows"
+    elif platform.system() == "Darwin":
+        return "Darwin"
+    elif platform.system() == "Linux":
+        return "Linux"
+    else:
+        print("Unknown OS. Unexpected results may occur.")
+        return "Unknown"
 
 
 def get_argparse() -> argparse.ArgumentParser:
+    default_input_videos_dir = VPDQ_DIR.parent / "tmk/sample-videos"
+
     ap = argparse.ArgumentParser(description=__doc__)
-    ap.add_argument(
-        "-f",
-        "--ffmpegPath",
-        metavar="FFMPEG_PATH",
-        help="Specific path to ffmpeg you want to use",
-        default="ffmpeg",
-    )
     ap.add_argument(
         "-r",
         "--secondsPerHash",
@@ -28,16 +45,15 @@ def get_argparse() -> argparse.ArgumentParser:
         "--outputHashFolder",
         metavar="OUTPUT_HASH_Folder_PATH",
         help="Output Hash Folder's Name",
-        default="/ThreatExchange/vpdq/output-hashes",
-        type=dir_path,
+        type=validate_path,
     )
     ap.add_argument(
         "-i",
         "--inputVideoFolder",
         metavar="INPUTPUT_VIDEO_FOLDER_PATH",
         help="Input Video Folder",
-        default="/ThreatExchange/tmk/sample-videos",
-        type=dir_path,
+        default=default_input_videos_dir,
+        type=validate_path,
     )
     ap.add_argument(
         "-s",
@@ -72,91 +88,135 @@ def get_argparse() -> argparse.ArgumentParser:
     return ap
 
 
-def dir_path(string):
-    if os.path.isdir(string):
-        return string
-    else:
-        raise argparse.ArgumentTypeError(f"readable_dir: {string} is not a valid path")
+def validate_path(path: Union[Path, str], err_msg: Union[str, None] = None) -> Path:
+    path = Path(path)
+    if not path.exists():
+        if err_msg is None:
+            err_msg = f"Path {path} does not exist."
+        raise argparse.ArgumentTypeError(err_msg)
+    return path
 
 
 def main():
+    OS = get_os()
+
+    hashVideoExecutable = EXEC_DIR / "vpdq-hash-video"
+    matchHashesExecutable = EXEC_DIR / "match-hashes-byline"
+
+    if OS == "Windows":
+        hashVideoExecutable = Path(f"{hashVideoExecutable}.exe")
+        matchHashesExecutable = Path(f"{matchHashesExecutable}.exe")
+
+    try:
+        validate_path(hashVideoExecutable)
+        validate_path(matchHashesExecutable)
+    except argparse.ArgumentTypeError as e:
+        print(e)
+        print(
+            "Error: Hashing executable/s not found. Build vpdq before running regtest."
+        )
+        sys.exit(1)
+
     ap = get_argparse()
     args = ap.parse_args()
     inputVideoFolder = args.inputVideoFolder
     outputHashFolder = args.outputHashFolder
-    ffmpegPath = args.ffmpegPath
     secondsPerHash = str(args.secondsPerHash)
     downsampleFrameDimension = str(args.downsampleFrameDimension)
-    verbose = args.verbose
-    # TODO: Add more general options for other video encodings.
-    for file in os.listdir(inputVideoFolder):
-        if file.endswith(".mp4"):
-            if verbose:
-                subprocess.call(
-                    [
-                        "./build/vpdq-hash-video",
-                        "-v",
-                        "-f",
-                        ffmpegPath,
-                        "-r",
-                        secondsPerHash,
-                        "-d",
-                        outputHashFolder,
-                        "-s",
-                        downsampleFrameDimension,
-                        "-i",
-                        inputVideoFolder + "/" + file,
-                    ]
-                )
-            else:
-                subprocess.call(
-                    [
-                        "./build/vpdq-hash-video",
-                        "-f",
-                        ffmpegPath,
-                        "-r",
-                        secondsPerHash,
-                        "-d",
-                        outputHashFolder,
-                        "-s",
-                        downsampleFrameDimension,
-                        "-i",
-                        inputVideoFolder + "/" + file,
-                    ]
-                )
-
-    cdir = os.getcwd()
-    pdir = os.path.dirname(cdir)
-    sample = pdir + ("/sample-hashes")
-    output = outputHashFolder
     distanceTolerance = str(args.matchDistanceTolerance)
     qualityTolerance = str(args.qualityTolerance)
-    for file in os.listdir(sample):
-        if file.endswith(".txt"):
-            print("\nMatching File " + file)
-            sampleFile = f"{sample}/{file}"
-            outputFile = f"{output}/{file}"
+    verbose = args.verbose
+
+    with TemporaryDirectory() as tempOutputHashFolder:
+        tempOutputHashFolder = Path(tempOutputHashFolder)
+
+        # Create output directory if it does not exist and it is specified
+        if outputHashFolder is not None:
+            if not outputHashFolder.exists():
+                print(f"Creating output directory at {outputHashFolder}")
+                outputHashFolder.mkdir(parents=True)
+            print(f"Writing output hash files to directory: {outputHashFolder}")
+        else:
+            print(f"Writing output to temp directory: {tempOutputHashFolder}")
+        # TODO: Add more general options for other video extensions.
+        for fileStr in glob.iglob(f"{inputVideoFolder}/**/*.mp4", recursive=True):
+            file = Path(fileStr)
+
+            # Create output hash file in a tempdir
+            outputHashFile = tempOutputHashFolder / f"{file.stem}.txt"
+            outputHashFile.touch(exist_ok=False)
+
+            print(f"\nHashing file {file.name}")
+            command = [
+                hashVideoExecutable,
+                "-r",
+                secondsPerHash,
+                "-d",
+                tempOutputHashFolder,
+                "-s",
+                downsampleFrameDimension,
+                "-i",
+                file,
+            ]
+
             if verbose:
-                subprocess.call(
-                    [
-                        "./build/match-hashes-byline",
-                        "-v",
-                        sampleFile,
-                        outputFile,
-                        distanceTolerance,
-                        qualityTolerance,
-                    ]
+                # Print all PDQHashes e.g.
+                # PDQHash: ebcc8b06b0666ea34cf9b85972a983a4f94668af05fc9d52aa9662f975499514
+                # selectframe 563
+                command.insert(1, "-v")
+
+            try:
+                hash_proc = subprocess.run(
+                    command,
+                    check=True,
+                    capture_output=True,
+                    shell=(OS == "Windows"),
                 )
-            else:
-                subprocess.call(
-                    [
-                        "./build/match-hashes-byline",
-                        sampleFile,
-                        outputFile,
-                        distanceTolerance,
-                        qualityTolerance,
-                    ]
+                print(str(hash_proc.stdout, "utf-8"))
+            except subprocess.CalledProcessError as e:
+                print(" ".join([str(i) for i in e.cmd]))
+                print(str(e.stderr, "utf-8"))
+                sys.exit(1)
+
+            # Copy hash files to output directory if it is specified
+            # This will overwrite existing files with the same
+            # name as outputHashFile in the directory
+            if outputHashFolder is not None:
+                copyfile(outputHashFile, Path(outputHashFolder / outputHashFile.name))
+        for outputFileStr in glob.iglob(
+            f"{tempOutputHashFolder}/**/*.txt", recursive=True
+        ):
+            outputFile = Path(outputFileStr)
+            sampleFile = Path(SAMPLE_HASHES_DIR / outputFile.name)
+            print(
+                f"\nMatching Video {sampleFile.name} from hash file {outputFile.name}"
+            )
+            command = [
+                matchHashesExecutable,
+                sampleFile,
+                outputFile,
+                distanceTolerance,
+                qualityTolerance,
+            ]
+
+            if verbose:
+                # Print all PDQHashes and if they match e.g.
+                # Line 201 Hash1: da4b380330b725b4a5f08ff03d0f6949da4fd2d3e7c8e4930fa7b80662a17c4e
+                # Hash2: da4b380330b725b4a5f08ff03d0f6949da4fd2d3e7c8e4930fa7b80662a17c4e match
+                command.insert(1, "-v")
+
+            try:
+                match_proc = subprocess.run(
+                    command,
+                    check=True,
+                    capture_output=True,
+                    shell=(OS == "Windows"),
                 )
+                print(str(match_proc.stdout, "utf-8"))
+            except subprocess.CalledProcessError as e:
+                print(e.cmd)
+                print(str(e.stderr, "utf-8"))
+                sys.exit(1)
 
 
 if __name__ == "__main__":
