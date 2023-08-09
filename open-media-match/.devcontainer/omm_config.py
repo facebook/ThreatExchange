51017f2f2152f7f7f4f1fe1358c7d38e7feeb291
Doug Neal <dougneal@meta.com>
2023-08-09 15:34:35 +0100

    OMM: Roles and blueprints (#1343)
    * [omm] Basic HTML index page, separate status endpoint
    
    The index endpoint ('/') is distinct in function from '/status',
    so separate it out. Add a basic HTML page here so that user is
    greeted with something human-readable to show they're up and running.
    
    * [omm] Define server roles with Flask blueprints for each role
    
    Define the hasher, matcher and curator roles. Allow them to be enabled
    or disabled via config. Separate those role functions out into discrete
    Flask "Blueprints" with URL prefixes for easy Layer 7 routing.
    Add stubs for a handful of example functions from the README.

--- open-media-match/.devcontainer/omm_config.py
+++ open-media-match/.devcontainer/omm_config.py
@@ -1,6 +1,11 @@
 # Database configuration
+PRODUCTION = False
 DBUSER = 'media_match'
 DBPASS = 'hunter2'
 DBHOST = 'db'
 DBNAME = 'media_match'
 DATABASE_URI = f"postgresql+psycopg2://{DBUSER}:{DBPASS}@{DBHOST}/{DBNAME}"
+
+ROLE_HASHER = True
+ROLE_MATCHER = True
+ROLE_CURATOR = True
--- open-media-match/src/OpenMediaMatch/__init__.py
+++ open-media-match/src/OpenMediaMatch/__init__.py
@@ -3,6 +3,7 @@
 import os
 import flask
 
+from .blueprints import hashing, matching, curation
 
 def create_app():
     """
@@ -10,23 +11,32 @@ def create_app():
     """
     app = flask.Flask(__name__)
     app.config.from_envvar('OMM_CONFIG')
+    
+    @app.route('/')
+    def index():
+        """
+        Sanity check endpoint showing a basic status page
+        TODO: in development mode, this could show some useful additional info
+        """
+        return flask.render_template('index.html.j2', production=app.config.get('PRODUCTION'))
 
-    @app.route("/")
-    def ping():
+    @app.route('/status')
+    def status():
         """
         Liveness/readiness check endpoint for your favourite Layer 7 load balancer
         """
-        return "I-AM-ALIVE"
+        return 'I-AM-ALIVE\n'
 
-    @app.route("/hash/image")
-    def hash_image():
-        """
-        Hash an image
-        """
-        image_url = flask.request.args.get('url')
-        if image_url is None:
-            flask.abort(400)
+    # Register Flask blueprints for whichever server roles are enabled...
+    # URL prefixing facilitates easy Layer 7 routing :) 
+    if app.config.get('ROLE_HASHER', False):
+        app.register_blueprint(hashing.bp, url_prefix='/h')
 
-        return f"If this was implemented you'd be seeing the PDQ hash of the image at {image_url} here"
+    if app.config.get('ROLE_MATCHER', False):
+        app.register_blueprint(matching.bp, url_prefix='/m')
+
+    if app.config.get('ROLE_MATCHER', False):
+        app.register_blueprint(curation.bp, url_prefix='/c')
+
 
     return app
