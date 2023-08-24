3b252a8b3faa08790924617aa7d69f982757ad35
Juan Mrad <juansmrad@gmail.com>
2023-08-24 14:48:40 -0500

    Setup first tests

---
+++ open-media-match/src/OpenMediaMatch/tests/test_api.py
@@ -0,0 +1,23 @@
+import os
+import pytest
+
+from OpenMediaMatch import create_app
+
+
+@pytest.fixture()
+def app():
+    os.environ.setdefault("OMM_CONFIG", "tests/omm_config.py")
+    app = create_app()
+
+    yield app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+def test_status_response(client):
+    response = client.get("/status")
+    assert response.status_code == 200
+    assert response.data == b"I-AM-ALIVE\n"
