a7a6dd0270f31e206ba56744b828a589903df63e
Juan Mrad <juanmrad@github.com>
2023-08-30 09:22:13 -0500

    [Open Media Matcher] GitHub Actions Setup (#1352)
    * Add dependencies for test and all
    
    * Configure mypy and dev container changes
    
    * Lint fixes from black
    
    * Setup first tests
    
    * Add initial setup for github actions
    
    * code review change: remove mypy.ini in favor of toml stanza

--- open-media-match/src/OpenMediaMatch/blueprints/hashing.py
+++ open-media-match/src/OpenMediaMatch/blueprints/hashing.py
@@ -3,26 +3,25 @@ from flask import abort, request
 
 bp = Blueprint("hashing", __name__)
 
-@bp.route('/hash')
+
+@bp.route("/hash")
 def hash_media():
     """
     Fetch content and return its hash.
     TODO: implement
     """
-    media_url = request.args.get('url', None)
+    media_url = request.args.get("url", None)
     if media_url is None:
         # path is required, otherwise we don't know what we're hashing.
         # TODO: a more helpful message
         abort(400)
-    
-    hash_types = request.args.get('types', None)
+
+    hash_types = request.args.get("types", None)
     if hash_types is not None:
         # TODO: parse this into a list of hash types
         pass
-
 
     # TODO
     #  - download the media
     #  - decode it
     #  - hash it
-    
