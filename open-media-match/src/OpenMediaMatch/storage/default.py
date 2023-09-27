f0cecd74cdc45594e3ddd9cf8c1454c0fc318673
dcallies <dcallies@meta.com>
2023-09-26 21:35:39 -0400

    [omm] Bank storage iface

--- open-media-match/src/OpenMediaMatch/storage/default.py
+++ open-media-match/src/OpenMediaMatch/storage/default.py
@@ -4,11 +4,23 @@
 The default store for accessing persistent data on OMM.
 """
 
+import typing as t
+
+from threatexchange.exchanges.signal_exchange_api import TSignalExchangeAPICls
+from threatexchange.signal_type.index import SignalTypeIndex
+from threatexchange.signal_type.signal_base import SignalType
+from threatexchange.exchanges.fetch_state import (
+    FetchCheckpointBase,
+    CollaborationConfigBase,
+)
+
+
 from OpenMediaMatch.storage import interface
-from OpenMediaMatch.storage import mocked
+from OpenMediaMatch.storage.mocked import MockedUnifiedStore
+from OpenMediaMatch.storage.interface import SignalTypeConfig
 
 
-class DefaultOMMStore(mocked.MockedUnifiedStore, interface.IUnifiedStore):
+class DefaultOMMStore(interface.IUnifiedStore):
     """
     The default store for accessing persistent data on OMM.
 
@@ -24,3 +36,48 @@ class DefaultOMMStore(mocked.MockedUnifiedStore, interface.IUnifiedStore):
       * PostGres-backed tables (e.g. info downloaded from external APIs)
       * Blobstore (e.g. built indices)
     """
+
+    def get_content_type_configs(self) -> t.Mapping[str, interface.ContentTypeConfig]:
+        # TODO
+        return MockedUnifiedStore().get_content_type_configs()
+
+    def get_exchange_type_configs(self) -> t.Mapping[str, TSignalExchangeAPICls]:
+        # TODO
+        return MockedUnifiedStore().get_exchange_type_configs()
+
+    def get_signal_type_configs(self) -> t.Mapping[str, SignalTypeConfig]:
+        # TODO
+        return MockedUnifiedStore().get_signal_type_configs()
+
+    # Index
+    def get_signal_type_index(
+        self, signal_type: type[SignalType]
+    ) -> t.Optional[SignalTypeIndex[int]]:
+        # TODO
+        return MockedUnifiedStore().get_signal_type_index(signal_type)
+
+    # Collabs
+    def get_collaborations(self) -> t.Dict[str, CollaborationConfigBase]:
+        # TODO
+        return MockedUnifiedStore().get_collaborations()
+
+    def get_collab_fetch_checkpoint(
+        self, collab: CollaborationConfigBase
+    ) -> t.Optional[FetchCheckpointBase]:
+        return MockedUnifiedStore().get_collab_fetch_checkpoint(collab)
+
+    def commit_collab_fetch_data(
+        self,
+        collab: CollaborationConfigBase,
+        dat: t.Dict[str, t.Any],
+        checkpoint: FetchCheckpointBase,
+    ) -> None:
+        MockedUnifiedStore().commit_collab_fetch_data(collab, dat, checkpoint)
+
+    def get_collab_data(
+        self,
+        collab_name: str,
+        key: str,
+        checkpoint: FetchCheckpointBase,
+    ) -> t.Any:
+        return MockedUnifiedStore().get_collab_data(collab_name, key, checkpoint)
