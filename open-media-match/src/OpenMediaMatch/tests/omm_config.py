ee36bb5c18b042c4cba6811e4f80b6e7abf5a6b1
Juan Mrad <juansmrad@gmail.com>
2023-10-01 14:37:19 -0500

    [omm] Configure test DB for pytest (#1382)

--- open-media-match/src/OpenMediaMatch/tests/omm_config.py
+++ open-media-match/src/OpenMediaMatch/tests/omm_config.py
@@ -3,7 +3,7 @@ PRODUCTION = False
 DBUSER = "media_match"
 DBPASS = "hunter2"
 DBHOST = "localhost"
-DBNAME = "media_match"
+DBNAME = "media_match_test"
 DATABASE_URI = f"postgresql+psycopg2://{DBUSER}:{DBPASS}@{DBHOST}/{DBNAME}"
 
 ROLE_HASHER = True
