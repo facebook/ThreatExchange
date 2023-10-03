9327f4bbc7041785b6517c89e694f46d82a42166
dtsytrytsyi <dmytro.tsytrytskyi@yahooinc.com>
2023-10-03 13:27:14 -0700

    Re-format file

--- open-media-match/src/OpenMediaMatch/app.py
+++ open-media-match/src/OpenMediaMatch/app.py
@@ -35,8 +35,7 @@ def create_app() -> flask.Flask:
         # the devcontainer and getting an error, just override the env
         app.config.from_pyfile("/workspace/.devcontainer/omm_config.py")
     else:
-        raise RuntimeError(
-            "No flask config given - try populating OMM_CONFIG env")
+        raise RuntimeError("No flask config given - try populating OMM_CONFIG env")
     app.config.update(
         SQLALCHEMY_DATABASE_URI=app.config.get("DATABASE_URI"),
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
@@ -101,16 +100,20 @@ def create_app() -> flask.Flask:
     def seed_data():
         """Insert plausible-looking data into the database layer"""
         from threatexchange.signal_type.pdq.signal import PdqSignal
+
         bankName = "TEST_BANK"
         contentList = []
         for example in PdqSignal.get_examples():
-            contentList.append(database.BankContent(
-                signals=[database.ContentSignal(
-                    signal_type=PdqSignal.get_name(),
-                    signal_val=example,
+            contentList.append(
+                database.BankContent(
+                    signals=[
+                        database.ContentSignal(
+                            signal_type=PdqSignal.get_name(),
+                            signal_val=example,
+                        )
+                    ]
                 )
-                ]
-            ))
+            )
         bank = database.Bank(
             name=bankName,
             content=contentList,
