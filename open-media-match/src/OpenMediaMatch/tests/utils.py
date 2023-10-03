0f7d935bdb5fe5b22ab64b3ebe98bfa96b504fb5
Juan Mrad <juansmrad@gmail.com>
2023-10-03 12:14:47 -0700

    linting

--- open-media-match/src/OpenMediaMatch/tests/utils.py
+++ open-media-match/src/OpenMediaMatch/tests/utils.py
@@ -7,18 +7,12 @@ from flask.testing import FlaskClient
 
 from OpenMediaMatch.app import create_app
 from OpenMediaMatch import database
-from flask import send_file
 
 
 @pytest.fixture()
 def app() -> t.Iterator[Flask]:
     os.environ.setdefault("OMM_CONFIG", "tests/omm_config.py")
     app = create_app()
-
-    @app.route("/get_image")
-    def get_image():
-        filename = "./data/b.jpg"
-        return send_file(filename, mimetype="image/jpeg")
 
     with app.app_context():
         # If we want to try and re-use the database between tests,
