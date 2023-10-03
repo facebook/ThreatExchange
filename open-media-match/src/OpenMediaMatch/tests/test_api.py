e5e59bcbd5806febfc0c9c68d6a2b11ca1aa39a8
Juan Mrad <juansmrad@gmail.com>
2023-10-03 11:31:33 -0700

    [omm] Allow to add content to bank

--- open-media-match/src/OpenMediaMatch/tests/test_api.py
+++ open-media-match/src/OpenMediaMatch/tests/test_api.py
@@ -26,3 +26,17 @@ def test_banks_create(client: FlaskClient):
     response = client.get("/c/banks")
     assert response.status_code == 200
     assert response.json == [post_response.json]
+
+
+def test_banks_add_hash(client: FlaskClient):
+    post_response = client.post(
+        "/c/banks",
+        json={"name": "MY_TEST_BANK"},
+    )
+    assert post_response.status_code == 201
+    assert post_response.json == {"matching_enabled_ratio": 1.0, "name": "MY_TEST_BANK"}
+
+    # Should now be visible on index
+    response = client.get("/c/banks")
+    assert response.status_code == 200
+    assert response.json == [post_response.json]
