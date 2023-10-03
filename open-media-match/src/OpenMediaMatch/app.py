a8a990573f6c800ee300f10975f7826ce71e610d
htracey-godaddy <htracey@godaddy.com>
2023-10-03 14:34:33 -0700

    adding signal and content types

--- open-media-match/src/OpenMediaMatch/app.py
+++ open-media-match/src/OpenMediaMatch/app.py
@@ -4,6 +4,8 @@ import logging
 import os
 import sys
 import warnings
+from src.OpenMediaMatch.blueprints.curation import get_all_signal_types
+from src.OpenMediaMatch.blueprints.curation import get_all_content_types
 
 import flask
 from flask.logging import default_handler
@@ -50,8 +52,10 @@ def create_app() -> flask.Flask:
         Sanity check endpoint showing a basic status page
         TODO: in development mode, this could show some useful additional info
         """
+        signaltypes = get_all_signal_types()
+        contenttypes = get_all_content_types()
         return flask.render_template(
-            "index.html.j2", production=app.config.get("PRODUCTION")
+            "index.html.j2", production=app.config.get("PRODUCTION"), signal=signaltypes, content=contenttypes
         )
 
     @app.route("/status")
