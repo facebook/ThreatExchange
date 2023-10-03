d033d53a83e6079cbd18780846fbc631788c0591
Juan Mrad <juansmrad@gmail.com>
2023-10-03 12:12:59 -0700

    [omm] add test for add bank content

--- open-media-match/src/OpenMediaMatch/tests/utils.py
+++ open-media-match/src/OpenMediaMatch/tests/utils.py
@@ -7,12 +7,18 @@ from flask.testing import FlaskClient
 
 from OpenMediaMatch.app import create_app
 from OpenMediaMatch import database
+from flask import send_file
 
 
 @pytest.fixture()
 def app() -> t.Iterator[Flask]:
     os.environ.setdefault("OMM_CONFIG", "tests/omm_config.py")
     app = create_app()
+
+    @app.route("/get_image")
+    def get_image():
+        filename = "./data/b.jpg"
+        return send_file(filename, mimetype="image/jpeg")
 
     with app.app_context():
         # If we want to try and re-use the database between tests,
@@ -27,3 +33,12 @@ def app() -> t.Iterator[Flask]:
 @pytest.fixture()
 def client(app) -> FlaskClient:
     return app.test_client()
+
+
+def create_bank(client: FlaskClient, bank_name: str):
+    post_response = client.post(
+        "/c/banks",
+        json={"name": bank_name},
+    )
+    assert post_response.status_code == 201
+    assert post_response.json == {"matching_enabled_ratio": 1.0, "name": bank_name}
