713fef1dd8854d313de214e68fcf475ef8b9fbad
Juan Mrad <juansmrad@gmail.com>
2023-10-04 16:58:28 -0500

    [OMM] UI for matching content (#1432)
    * [OMM] UI for matching content
    
    * linter

--- open-media-match/src/OpenMediaMatch/blueprints/ui.py
+++ open-media-match/src/OpenMediaMatch/blueprints/ui.py
@@ -5,7 +5,7 @@ import requests
 from flask import Blueprint, abort
 from flask import request, redirect, url_for
 
-from OpenMediaMatch.blueprints import matching, curation
+from OpenMediaMatch.blueprints import matching, curation, hashing
 
 bp = Blueprint("ui", __name__)
 
@@ -23,33 +23,14 @@ def ui_create_bank():
 
 @bp.route("/query", methods=["POST"])
 def upload():
-    signaltypes = curation.get_all_signal_types()
-    contenttypes = curation.get_all_content_types()
-    banks = curation.banks_index()
-    # content type from dropdown form
-    contenttype = request.form.get("media")
-    f = request.files["file"]
-    f.save(f.filename)
-    files = {
-        contenttype: open(f.filename, "rb"),
-    }
-    # returns a dictionary of {'signaltype' : 'hash'}
+    hash = hashing.hash_media_post()
+    # The hash function returns an object with a single key (the signal_type) and value (the signal)
+    signal_type = list(hash.keys())[0]
+    signal = hash[signal_type]
 
-    r = requests.post("http://localhost:5000/h/hash", files=files)
-    rjson = r.json()
-    for key, value in rjson.items():
-        matches = matching.lookup_signal(value, key)
-        matcheslist = matches["matches"]
-    os.remove(f.filename)
-    return flask.render_template(
-        "index.html.j2",
-        name=f.filename,
-        matches=matcheslist,
-        fileresult=True,
-        signal=signaltypes,
-        content=contenttypes,
-        bankList=banks,
-    )
+    banks = matching.lookup(signal, signal_type)
+
+    return {"hashes": hash, "banks": banks}
 
 
 @bp.route("/addbank", methods=["POST"])
