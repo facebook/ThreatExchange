4f35f6778c62417d0d7fc3b18754a3ad6388f7b0
sbruens <sbruens@users.noreply.github.com>
2023-10-11 15:47:09 -0400

    Leave gunicorn config in devcontainer and make a new one for prod.

--- open-media-match/.devcontainer/gunicorn_omm_config.py
+++ open-media-match/.devcontainer/gunicorn_omm_config.py
@@ -1,17 +1,23 @@
+"""
+A
+
+This is meant to demonstrate the differences between the development
+"""
+
 from threatexchange.signal_type.pdq.signal import PdqSignal
 from threatexchange.signal_type.md5 import VideoMD5Signal
 
 # Database configuration
-PRODUCTION = True
+PRODUCTION = False
 DBUSER = "media_match"
 DBPASS = "hunter2"
-DBHOST = "db"
+DBHOST = "localhost"
 DBNAME = "media_match"
 DATABASE_URI = f"postgresql+psycopg2://{DBUSER}:{DBPASS}@{DBHOST}/{DBNAME}"
 
 # Role configuration
-ROLE_HASHER = True
-ROLE_MATCHER = True
+ROLE_HASHER = False
+ROLE_MATCHER = False
 ROLE_CURATOR = True
 
 # Installed SignalTypes
