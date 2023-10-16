06d3026e4309833b76c4ef722325a6095995f1d8
Sander Bruens <sbruens@users.noreply.github.com>
2023-10-16 10:30:09 -0400

    [omm] Create Compose file for gunicorn production deployment. (#1446)

---
+++ open-media-match/omm_config.py
@@ -0,0 +1,19 @@
+import os
+from threatexchange.signal_type.pdq.signal import PdqSignal
+from threatexchange.signal_type.md5 import VideoMD5Signal
+
+# Database configuration
+PRODUCTION = True
+DBUSER = os.environ.get("POSTGRES_USER", "media_match")
+DBPASS = os.environ.get("POSTGRES_PASSWORD", "hunter2")
+DBHOST = "db"
+DBNAME = "media_match"
+DATABASE_URI = f"postgresql+psycopg2://{DBUSER}:{DBPASS}@{DBHOST}/{DBNAME}"
+
+# Role configuration
+ROLE_HASHER = True
+ROLE_MATCHER = True
+ROLE_CURATOR = True
+
+# Installed SignalTypes
+SIGNAL_TYPES = [PdqSignal, VideoMD5Signal]
