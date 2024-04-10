35adae95a26e63fe3785198006cc450c7b240dab
dcallies <dcallies@meta.com>
2024-04-10 17:09:06 -0400

    progress

--- hasher-matcher-actioner/src/OpenMediaMatch/static/js/autoreload.js
+++ hasher-matcher-actioner/src/OpenMediaMatch/static/js/autoreload.js
@@ -1,6 +1,8 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
 // Reload the page automagically 
-setTimeout(function(){
+setTimeout(function () {
     window.location.reload();
- }, 
- 240000  // 4 min in ms
- );
+},
+    240000  // 4 min in ms
+);
