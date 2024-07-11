fc072b526fa0bed8faad5dd946251c9d3bd089f8
Arya Seghatoleslami <arya.seghatoleslami@gmail.com>
2024-07-11 19:23:36 -0400

    fix create exchange API selector

--- hasher-matcher-actioner/.devcontainer/startup.sh
+++ hasher-matcher-actioner/.devcontainer/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 export OMM_CONFIG=/workspace/reference_omm_configs/development_omm_config.py
-MIGRATION_COMMAND=1 flask --app OpenMediaMatch.app db upgrade --directory /workspace/src/OpenMediaMatch/migrations
-flask --app OpenMediaMatch.app run --debug
+MIGRATION_COMMAND=1 flask --app src/OpenMediaMatch.app db upgrade --directory /workspace/src/OpenMediaMatch/migrations
+flask --app src/OpenMediaMatch.app run --debug
