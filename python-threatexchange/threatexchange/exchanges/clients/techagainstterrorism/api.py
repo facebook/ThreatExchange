34816196d94b918f5d10b4d9ebaceedb302fc51d
Bruce Pouncey <bruce.pouncey@quantspark.com>
2024-09-05 10:30:54 +0100

    issue_1610: Attempted backwards compatability type hints | black formatting

--- python-threatexchange/threatexchange/exchanges/clients/techagainstterrorism/api.py
+++ python-threatexchange/threatexchange/exchanges/clients/techagainstterrorism/api.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
 import logging
-from typing import Any, ClassVar, Optional
+import typing as t
 from contextlib import contextmanager
 import requests
 from requests.packages.urllib3.util.retry import Retry
@@ -16,160 +16,153 @@ from threatexchange.exchanges.clients.fb_threatexchange.api import TimeoutHTTPAd
 
 
 class TATIdeology(Enum):
-  islamist = "islamist"
-  far_right = "far-right"
-  _all = "all"
+    islamist = "islamist"
+    far_right = "far-right"
+    _all = "all"
 
 
 @dataclass
 class TATAPIErrorResponse:
-  error: str
+    error: str
+
 
 @dataclass
 class TATHashListResponse:
-  file_url: str
-  file_name: str
-  created_on: datetime
-  total_hashes: int
-  ideology: TATIdeology
+    file_url: str
+    file_name: str
+    created_on: datetime
+    total_hashes: int
+    ideology: t.Union[
+        TATIdeology._all.value, TATIdeology.far_right.value, TATIdeology.islamist.value
+    ]
+
 
 @dataclass
 class TATUser:
-  user: Any
-  token: str
+    user: t.Any
+    token: str
+
 
 class TATEndpoint(Enum):
-  authenticate = "token-auth/tcap/"
-  hash_list = "api/hash-list"
+    authenticate = "token-auth/tcap/"
+    hash_list = "api/hash-list"
 
 
 class TATHashListAPI:
-  """
-  A wrapper around the Tech Against Terrorism Hash List API
-
-  The verification and collection of terrorist content are conducted by TAT OSINT Analysts and automated processes. 
-  Subsequently, the content is classified and hashed by the TAT Archive hashing and classification services.
-
-  This API delivers a JSON file containing a comprehensive list of all hashed terrorist content within the TAT system.
-
-  The list is refreshed daily.
-  """
-
-  BASE_URL: ClassVar[str] = "https://beta.terrorismanalytics.org/"
-
-  def __init__(
-      self, 
-      username: str,
-      password: str
-    ) -> None:
-    self.username = username
-    self.password = password
-
-
-  @contextmanager
-  def _get_session(self, auth_token: Optional[str] = None):
-    session = requests.Session()
-    session.mount(
-      self.BASE_URL,
-      adapter=TimeoutHTTPAdapter(
-        timeout=60,
-        max_retries=Retry(
-          total=4,
-          status_forcelist=[429, 500, 502, 503, 504],
-          allowed_methods=["HEAD", "GET", "OPTIONS"],
-          backoff_factor=0.2,  # ~1.5 seconds of retries
-        ),
-      )
-    )
-
-    if auth_token:
-      session.headers.update({"Authorization": f"Bearer {auth_token}"})
-
-    try:
-      yield session
-    finally:
-      session.close()
-
-
-  def _get(self, endpoint: TATEndpoint, auth_token: Optional[str] = None) -> Any:
     """
+    A wrapper around the Tech Against Terrorism Hash List API
+
+    The verification and collection of terrorist content are conducted by TAT OSINT Analysts and automated processes.
+    Subsequently, the content is classified and hashed by the TAT Archive hashing and classification services.
+
+    This API delivers a JSON file containing a comprehensive list of all hashed terrorist content within the TAT system.
+
+    The list is refreshed daily.
+    """
+
+    BASE_URL: t.ClassVar[str] = "https://beta.terrorismanalytics.org/"
+
+    def __init__(self, username: str, password: str) -> None:
+        self.username = username
+        self.password = password
+
+    @contextmanager
+    def _get_session(self, auth_token: t.Optional[str] = None):
+        session = requests.Session()
+        session.mount(
+            self.BASE_URL,
+            adapter=TimeoutHTTPAdapter(
+                timeout=60,
+                max_retries=Retry(
+                    total=4,
+                    status_forcelist=[429, 500, 502, 503, 504],
+                    allowed_methods=["HEAD", "GET", "OPTIONS"],
+                    backoff_factor=0.2,  # ~1.5 seconds of retries
+                ),
+            ),
+        )
+
+        if auth_token:
+            session.headers.update({"Authorization": f"Bearer {auth_token}"})
+
+        try:
+            yield session
+        finally:
+            session.close()
+
+    def _get(self, endpoint: str, auth_token: t.Optional[str] = None) -> t.Any:
+        """
         Perform an HTTP GET request, and return the JSON response payload.
 
         Same timeouts and retry strategy as `_get_session` above.
-    """
-    with self._get_session(auth_token) as session:
-      url = self.BASE_URL + endpoint.value
-      response = session.get(url=url)
-      response.raise_for_status()
-      return response.json()
+        """
+        with self._get_session(auth_token) as session:
+            url = self.BASE_URL + endpoint
+            response = session.get(url=url)
+            response.raise_for_status()
+            return response.json()
 
-
-  def _post(self, endpoint: TATEndpoint, data=None) -> Any:
-    """
+    def _post(self, endpoint: TATEndpoint, data=None) -> t.Any:
+        """
         Perform an HTTP POST request, and return the JSON response payload.
 
         Same timeouts and retry strategy as `_get_session` above.
-    """
-    with self._get_session() as session:
-      url = self.BASE_URL + endpoint.value
-      response = session.post(url=url, data=data)
-      response.raise_for_status()
-      return response.json()
+        """
+        with self._get_session() as session:
+            url = self.BASE_URL + endpoint.value
+            response = session.post(url=url, data=data)
+            response.raise_for_status()
+            return response.json()
 
+    def authenticate(self, username: str, password: str) -> t.Optional[str]:
+        """
+        Authenticate with TCAP services and obtain a JWT token
+        """
 
-  def authenticate(self, username: str, password: str) -> str | None:
-    """
-    Authenticate with TCAP services and obtain a JWT token
-    """
+        if not isinstance(username, str) or not isinstance(password, str):
+            logging.error("Username or password not valid")
+            return None
 
-    if not isinstance(username, str) or not isinstance(password, str):
-      logging.error("Username or password not valid")
-      return None
+        logging.info("Authenticating with TCAP: %s", username)
 
-    logging.info("Authenticating with TCAP: %s", username)
+        try:
+            auth_response = self._post(
+                TATEndpoint.authenticate,
+                data={"username": username, "password": password, "resend": False},
+            )
+            return auth_response.get("token")
 
+        except Exception as e:
+            logging.error("Error authenticating with TCAP: %s", e)
+            return None
 
-    try:
-      auth_response = self._post(
-        TATEndpoint.authenticate, 
-        data={"username": username, "password": password, "resend": False}
-      )
-      return auth_response.get("token")
-    
-    except Exception as e:
-      logging.error("Error authenticating with TCAP: %s", e)
-      return None
+    def get_hash_list(
+        self, ideology: str = TATIdeology._all.value
+    ) -> t.Union[TATHashListResponse, TATAPIErrorResponse]:
+        """
+        Get the Hash List JSON file presigned URL ( 5 Minute expiry ) and metadata: TATHashListResponse
+        """
 
+        try:
+            token = self.authenticate(self.username, self.password)
 
+            endpoint = TATEndpoint.hash_list.value + ideology
 
-  def get_hash_list(self, ideology: TATIdeology = TATIdeology._all) -> TATHashListResponse | TATAPIErrorResponse:
-    """
-    Get the Hash List JSON file presigned URL ( 5 Minute expiry ) and metadata: TATHashListResponse
-    """
+            print("ENDPOINT", endpoint)
 
-    try:
-      token = self.authenticate(self.username, self.password)
+            if token is not None:
+                logging.info("Fetching hash list")
+                response = self._get(endpoint, auth_token=token)
 
-      if token is not None:
-        logging.info("Fetching hash list")
-        response = self._get(
-          TATEndpoint.hash_list,
-          auth_token=token
-        )
-        
-        return TATHashListResponse(**response)
-    
-      else:
-        logging.error("Error getting hash list: %s", e)
-        raise Exception("Unable authenticating with TCAP")
+                return TATHashListResponse(**response)
 
-    except requests.exceptions.HTTPError as http_err:
-        return TATAPIErrorResponse(error=str(http_err))
-      
-      
-    except Exception as e:
-      logging.error("Error getting hash list: %s", e)
-      return TATAPIErrorResponse(error=str(e))
-    
+            else:
+                logging.error("Authentication failed")
+                raise Exception("Unable authenticating with TCAP")
 
+        except requests.exceptions.HTTPError as http_err:
+            return TATAPIErrorResponse(error=str(http_err))
 
+        except Exception as e:
+            logging.error("Error getting hash list: %s", e)
+            return TATAPIErrorResponse(error=str(e))
--- python-threatexchange/threatexchange/exchanges/clients/techagainstterrorism/tests/test_api.py
+++ python-threatexchange/threatexchange/exchanges/clients/techagainstterrorism/tests/test_api.py
@@ -1,22 +1,29 @@
 import typing as t
 import pytest
+from datetime import datetime
+
 from threatexchange.exchanges.clients.techagainstterrorism.api import (
-   TATIdeology,
-   TATHashListResponse,
-   TATHashListAPI
+    TATIdeology,
+    TATHashListResponse,
+    TATHashListAPI,
 )
 
-def mock_get_hash_list(ideology: str = TATIdeology._all.value) -> t.Union[TATHashListResponse, dict[str, str]]:
-    
+
+def mock_get_hash_list(
+    ideology: str = TATIdeology._all.value,
+) -> t.Union[TATHashListResponse, dict[str, str]]:
+
     if ideology not in TATIdeology._value2member_map_:
-        return {'error': f'400 Client Error: Bad Request for url: https://test/api/hash-list/{ideology}'}
+        return {
+            "error": f"400 Client Error: Bad Request for url: https://test/api/hash-list/{ideology}"
+        }
 
     return TATHashListResponse(
-      file_url=f"https://hash-list.s3.com/19700101_{ideology}_hashes.json",
-      file_name=f"19790101_{ideology}_hashes.json",
-      created_on="2021-08-01T00:00:00Z",
-      total_hashes=1000,
-      ideology=ideology
+        file_url=f"https://hash-list.s3.com/19700101_{ideology}_hashes.json",
+        file_name=f"19790101_{ideology}_hashes.json",
+        created_on=datetime.strptime("2021-08-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"),
+        total_hashes=1000,
+        ideology=ideology,
     )
 
 
@@ -36,11 +43,11 @@ def test_get_islamist_hash_list(api: TATHashListAPI) -> None:
     response = api.get_hash_list(ideology=TATIdeology.islamist.value)
 
     assert response == TATHashListResponse(
-      file_url=f"https://hash-list.s3.com/19700101_{TATIdeology.islamist.value}_hashes.json",
-      file_name=f"19790101_{TATIdeology.islamist.value}_hashes.json",
-      created_on="2021-08-01T00:00:00Z",
-      total_hashes=1000,
-      ideology=TATIdeology.islamist.value
+        file_url=f"https://hash-list.s3.com/19700101_islamist_hashes.json",
+        file_name=f"19790101_islamist_hashes.json",
+        created_on=datetime.strptime("2021-08-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"),
+        total_hashes=1000,
+        ideology=TATIdeology.islamist.value,
     )
 
 
@@ -48,11 +55,11 @@ def test_get_far_right_hash_list(api: TATHashListAPI) -> None:
     response = api.get_hash_list(ideology=TATIdeology.far_right.value)
 
     assert response == TATHashListResponse(
-      file_url=f"https://hash-list.s3.com/19700101_{TATIdeology.far_right.value}_hashes.json",
-      file_name=f"19790101_{TATIdeology.far_right.value}_hashes.json",
-      created_on="2021-08-01T00:00:00Z",
-      total_hashes=1000,
-      ideology=TATIdeology.far_right.value
+        file_url=f"https://hash-list.s3.com/19700101_far-right_hashes.json",
+        file_name=f"19790101_far-right_hashes.json",
+        created_on=datetime.strptime("2021-08-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"),
+        total_hashes=1000,
+        ideology=TATIdeology.far_right.value,
     )
 
 
@@ -60,11 +67,11 @@ def test_get_all_hash_list(api: TATHashListAPI) -> None:
     response = api.get_hash_list(TATIdeology._all.value)
 
     assert response == TATHashListResponse(
-      file_url=f"https://hash-list.s3.com/19700101_{TATIdeology._all.value}_hashes.json",
-      file_name=f"19790101_{TATIdeology._all.value}_hashes.json",
-      created_on="2021-08-01T00:00:00Z",
-      total_hashes=1000,
-      ideology=TATIdeology._all.value
+        file_url=f"https://hash-list.s3.com/19700101_all_hashes.json",
+        file_name=f"19790101_all_hashes.json",
+        created_on=datetime.strptime("2021-08-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"),
+        total_hashes=1000,
+        ideology=TATIdeology._all.value,
     )
 
 
@@ -72,14 +79,16 @@ def test_get_default_hash_list(api: TATHashListAPI) -> None:
     response = api.get_hash_list()
 
     assert response == TATHashListResponse(
-      file_url=f"https://hash-list.s3.com/19700101_{TATIdeology._all.value}_hashes.json",
-      file_name=f"19790101_{TATIdeology._all.value}_hashes.json",
-      created_on="2021-08-01T00:00:00Z",
-      total_hashes=1000,
-      ideology=TATIdeology._all.value
+        file_url=f"https://hash-list.s3.com/19700101_all_hashes.json",
+        file_name=f"19790101_all_hashes.json",
+        created_on=datetime.strptime("2021-08-01T00:00:00Z", "%Y-%m-%dT%H:%M:%SZ"),
+        total_hashes=1000,
+        ideology=TATIdeology._all.value,
     )
 
 
 def test_incorrect_ideology_hash_list(api: TATHashListAPI) -> None:
     response = api.get_hash_list("invalid_ideology")
-    assert response == {'error': '400 Client Error: Bad Request for url: https://test/api/hash-list/invalid_ideology'}
+    assert response == {
+        "error": "400 Client Error: Bad Request for url: https://test/api/hash-list/invalid_ideology"
+    }
