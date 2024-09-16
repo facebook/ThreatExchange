b00ae0842188fac743d5d824d1542fd7e1205bb8
Bruce Pouncey <bruce.pouncey@quantspark.com>
2024-09-16 11:37:19 +0100

    enforced checking if MD5 hash is from a video

--- python-threatexchange/threatexchange/exchanges/impl/techagainstterrorism_api.py
+++ python-threatexchange/threatexchange/exchanges/impl/techagainstterrorism_api.py
@@ -105,7 +105,12 @@ class TATSignalExchangeAPI(
 
 
 def _is_compatible_signal_type(record: t.Dict[str, str]) -> bool:
-    return record["file_type"] in ["mov", "m4v", "mp4"] or record["algorithm"] == "PDQ"
+    compatible_video_types = ["mov", "m4v", "mp4", "webm"]
+
+    if record["algorithm"] == "MD5":
+        return record["file_type"] in compatible_video_types
+
+    return record["algorithm"] == "PDQ"
 
 
 def _type_mapping() -> t.Dict[str, str]:
