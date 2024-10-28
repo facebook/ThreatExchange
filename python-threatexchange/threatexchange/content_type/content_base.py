26fe24655c96e273c45db16b26050fd65d3c695a
haianhng31 <anguyen137@fordham.edu>
2024-10-28 21:01:18 +0000

    Create RotationType enum

--- python-threatexchange/threatexchange/content_type/content_base.py
+++ python-threatexchange/threatexchange/content_type/content_base.py
@@ -7,6 +7,7 @@ Abstraction for different content types.
 This records all the valid signal types for a piece of content.
 """
 
+from enum import StrEnum, auto
 import typing as t
 
 from threatexchange import common
@@ -32,3 +33,13 @@ class ContentType:
         * Video => break out photo thumbnail, close caption text, audio
         """
         return []
+
+class RotationType(StrEnum):
+    ORIGINAL = auto() # No rotation; the object is in its original orientation
+    ROTATE90 = auto() # Rotates the object 90 degrees 
+    ROTATE180 = auto() # Rotates the object 180 degrees (half-turn)
+    ROTATE270 = auto() # Rotates the object 270 degrees 
+    FLIPX = auto() # Flip the object horizontally along the X-axis 
+    FLIPY = auto() # Flip the object horizontally along the Y-axis 
+    FLIPPLUS1 = auto() # Diagonal flip along the line y = x
+    FLIPMINUS1 = auto() # Diagonal flip along the line y = -x
