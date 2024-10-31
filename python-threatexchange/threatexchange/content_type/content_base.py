67bbcd22255da3a832e30983a18f6107f2ce1a5e
haianhng31 <anguyen137@fordham.edu>
2024-10-31 17:05:02 +0000

    Change StrEnum to Enum

--- python-threatexchange/threatexchange/content_type/content_base.py
+++ python-threatexchange/threatexchange/content_type/content_base.py
@@ -7,7 +7,7 @@ Abstraction for different content types.
 This records all the valid signal types for a piece of content.
 """
 
-from enum import StrEnum, auto
+from enum import Enum, auto
 import typing as t
 
 from threatexchange import common
@@ -35,18 +35,18 @@ class ContentType:
         return []
 
 
-class RotationType(StrEnum):
+class RotationType(Enum):
     """
     Enum for 8 simple rotations of an image.
     Used to store all generated rotations of an image,
     whose algorithms don't have a native way to generate rotations during hashing.
     """
 
-    ORIGINAL = auto()  # No rotation; the object is in its original orientation
-    ROTATE90 = auto()  # Rotates the object 90 degrees
-    ROTATE180 = auto()  # Rotates the object 180 degrees (half-turn)
-    ROTATE270 = auto()  # Rotates the object 270 degrees
-    FLIPX = auto()  # Flip the object horizontally along the X-axis
-    FLIPY = auto()  # Flip the object horizontally along the Y-axis
-    FLIPPLUS1 = auto()  # Diagonal flip along the line y = x
-    FLIPMINUS1 = auto()  # Diagonal flip along the line y = -x
+    ORIGINAL = "original"  # No rotation; the object is in its original orientation
+    ROTATE90 = "rotate90"  # Rotates the object 90 degrees
+    ROTATE180 = "rotate180"  # Rotates the object 180 degrees (half-turn)
+    ROTATE270 = "rotate270"  # Rotates the object 270 degrees
+    FLIPX = "flipx"  # Flip the object horizontally along the X-axis
+    FLIPY = "flipy"  # Flip the object horizontally along the Y-axis
+    FLIPPLUS1 = "flipplus1"  # Diagonal flip along the line y = x
+    FLIPMINUS1 = "flipminus1"  # Diagonal flip along the line y = -x
