ce8547456c1f09b81116f816cd881d2af439c3f7
Hennessy Nguyen <126405175+haianhng31@users.noreply.github.com>
2024-11-01 12:11:14 -0400

    [py-tx] Create rotation helper functions in PhotoContent using Pillow (#1671)

--- python-threatexchange/threatexchange/content_type/content_base.py
+++ python-threatexchange/threatexchange/content_type/content_base.py
@@ -7,6 +7,7 @@ Abstraction for different content types.
 This records all the valid signal types for a piece of content.
 """
 
+from enum import Enum, auto
 import typing as t
 
 from threatexchange import common
@@ -32,3 +33,20 @@ class ContentType:
         * Video => break out photo thumbnail, close caption text, audio
         """
         return []
+
+
+class RotationType(Enum):
+    """
+    Enum for 8 simple rotations of an image.
+    Used to store all generated rotations of an image,
+    whose algorithms don't have a native way to generate rotations during hashing.
+    """
+
+    ORIGINAL = "original"  # No rotation; the object is in its original orientation
+    ROTATE90 = "rotate90"  # Rotates the object 90 degrees
+    ROTATE180 = "rotate180"  # Rotates the object 180 degrees (half-turn)
+    ROTATE270 = "rotate270"  # Rotates the object 270 degrees
+    FLIPX = "flipx"  # Flip the object horizontally along the X-axis
+    FLIPY = "flipy"  # Flip the object horizontally along the Y-axis
+    FLIPPLUS1 = "flipplus1"  # Diagonal flip along the line y = x
+    FLIPMINUS1 = "flipminus1"  # Diagonal flip along the line y = -x
--- python-threatexchange/threatexchange/content_type/photo.py
+++ python-threatexchange/threatexchange/content_type/photo.py
@@ -4,8 +4,10 @@
 """
 Wrapper around the video content type.
 """
+from PIL import Image
+import io
 
-from .content_base import ContentType
+from .content_base import ContentType, RotationType
 
 
 class PhotoContent(ContentType):
@@ -19,3 +21,83 @@ class PhotoContent(ContentType):
       * frames from videos
       * thumbnails of videos
     """
+
+    @classmethod
+    def rotate_image(cls, image_data: bytes, angle: float) -> bytes:
+        """
+        Rotate an image by a given angle.
+        """
+        with Image.open(io.BytesIO(image_data)) as img:
+            rotated_img = img.rotate(angle, expand=True)
+            with io.BytesIO() as buffer:
+                rotated_img.save(buffer, format=img.format)
+                return buffer.getvalue()
+
+    @classmethod
+    def flip_x(cls, image_data: bytes) -> bytes:
+        """
+        Flip the image horizontally along the X-axis.
+        """
+        with Image.open(io.BytesIO(image_data)) as img:
+            flipped_img = img.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+            with io.BytesIO() as buffer:
+                flipped_img.save(buffer, format=img.format)
+                return buffer.getvalue()
+
+    @classmethod
+    def flip_y(cls, image_data: bytes) -> bytes:
+        """
+        Flip the image vertically along the Y-axis.
+        """
+        with Image.open(io.BytesIO(image_data)) as img:
+            flipped_img = img.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+            with io.BytesIO() as buffer:
+                flipped_img.save(buffer, format=img.format)
+                return buffer.getvalue()
+
+    @classmethod
+    def flip_plus1(cls, image_data: bytes) -> bytes:
+        """
+        Flip the image diagonally along the line y = x.
+        """
+        with Image.open(io.BytesIO(image_data)) as img:
+            flipped_img = img.transpose(Image.Transpose.ROTATE_270).transpose(
+                Image.Transpose.FLIP_LEFT_RIGHT
+            )
+            with io.BytesIO() as buffer:
+                flipped_img.save(buffer, format=img.format)
+                return buffer.getvalue()
+
+    @classmethod
+    def flip_minus1(cls, image_data: bytes) -> bytes:
+        """
+        Flip the image diagonally along the line y = -x.
+        """
+        with Image.open(io.BytesIO(image_data)) as img:
+            flipped_img = img.transpose(Image.Transpose.ROTATE_90).transpose(
+                Image.Transpose.FLIP_LEFT_RIGHT
+            )
+            with io.BytesIO() as buffer:
+                flipped_img.save(buffer, format=img.format)
+                return buffer.getvalue()
+
+    @classmethod
+    def all_simple_rotations(cls, image_data: bytes):
+        """
+        Generate the 8 naive rotations of an image.
+
+        This can be helpful for testing.
+        And for image algorithms that don't have a native way to generate rotations during hashing,
+        this can be a way to brute force rotations.
+        """
+        rotations = {
+            RotationType.ORIGINAL: image_data,
+            RotationType.ROTATE90: cls.rotate_image(image_data, 90),
+            RotationType.ROTATE180: cls.rotate_image(image_data, 180),
+            RotationType.ROTATE270: cls.rotate_image(image_data, 270),
+            RotationType.FLIPX: cls.flip_x(image_data),
+            RotationType.FLIPY: cls.flip_y(image_data),
+            RotationType.FLIPPLUS1: cls.flip_plus1(image_data),
+            RotationType.FLIPMINUS1: cls.flip_minus1(image_data),
+        }
+        return rotations
