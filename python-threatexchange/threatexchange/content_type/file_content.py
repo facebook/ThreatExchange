8aff837d930fd96a3d587ef40e25517f97f6fa18
ZeyadTarekk <zeyad.ta01@gmail.com>
2024-11-08 14:35:52 +0200

    Change the valid photo types

--- python-threatexchange/threatexchange/content_type/file_content.py
+++ python-threatexchange/threatexchange/content_type/file_content.py
@@ -2,29 +2,29 @@ import typing as t
 from content_base import ContentType
 from photo import PhotoContent
 from video import VideoContent
+from PIL import Image
 
 class FileContent(ContentType):
     """
-    Content representing a file. Determines if a file is a photo or video based on file extension.
+    ContentType representing a generic file. 
+    
+    Determines if a file is a photo or video based on file extension.
     """
 
-    VALID_PHOTO_EXTENSIONS = {".jpg", ".jpeg", ".png", ".gif"}
+    VALID_PHOTO_EXTENSIONS = {ext.lower() for ext in Image.registered_extensions()}
     VALID_VIDEO_EXTENSIONS = {".mp4", ".avi", ".mov", ".mkv"}
 
-    def __init__(self, file_name: str):
-        self.file_name = file_name
-
     @classmethod
-    def get_name(cls) -> str:
-        return "File"
-
-    def get_content_type_from_filename(self) -> t.Type[ContentType]:
+    def get_content_type_from_filename(cls, file_name: str) -> t.Type[ContentType]:
         """
         Determines content type based on file extension.
         """
-        if any(self.file_name.endswith(ext) for ext in self.VALID_PHOTO_EXTENSIONS):
+        file_extension = file_name.lower().rsplit('.', 1)[-1]
+        file_extension = f".{file_extension}"
+
+        if file_extension in cls.VALID_PHOTO_EXTENSIONS:
             return PhotoContent
-        elif any(self.file_name.endswith(ext) for ext in self.VALID_VIDEO_EXTENSIONS):
+        elif file_extension in cls.VALID_VIDEO_EXTENSIONS:
             return VideoContent
         else:
-            raise ValueError(f"Unknown content type for file: {self.file_name}")
+            return None
