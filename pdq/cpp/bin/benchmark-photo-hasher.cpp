1f42b67df3da64d9b96af1d4e092dfe846631320
root <ericzh1616@gmail.com>
2024-12-03 16:08:14 +0000

    init untested

---
+++ pdq/cpp/bin/benchmark-photo-hasher.cpp
@@ -0,0 +1,145 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pdq/cpp/io/pdqio.h>
+#include <pdq/cpp/io/hashio.h>
+
+#include <iostream>
+#include <filesystem>
+#include <chrono>
+
+using namespace facebook::pdq::hashing;
+
+// ================================================================
+// Static function declarations
+static void usage(char* argv0, int rc);
+static void hash(char* argv0, int argc, char** argv);
+
+// ----------------------------------------------------------------
+int main(int argc, char** argv) {
+  if (argc > 1 && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help"))) {
+    usage(argv[0], 0);
+  } else {
+    hash(argv[0], argc - 1, argv + 1);
+  }
+  return 0;
+}
+
+// ----------------------------------------------------------------
+static void usage(char* argv0, int rc) {
+  FILE* fp = (rc == 0) ? stdout : stderr;
+  fprintf(fp, "Usage: %s [options] folder_path\n", argv0);
+  fprintf(fp, "Options:\n");
+  fprintf(fp, "  -v               Verbose output\n");
+  fprintf(fp, "  -n N             Total number of hashes to generate, can be more or less than the number of images in the folder\n");
+  fprintf(fp, "                           (default: 0, meaning generate one hash for each image in the folder)\n");
+  fprintf(fp, "  --dihedral       Compute dihedral versions of the hashes (default: false)\n");
+  exit(rc);
+}
+
+static void hash(char* argv0, int argc, char** argv) {
+  std::string folderPath;
+  int numHashes = 0;
+  bool verbose = false;
+  bool dihedral = false; // TODO:
+
+  // Parse command line arguments // TODO:
+  for (int i = 0; i < argc; i++) {
+    std::string arg = argv[i];
+    if (arg == "-v") {
+      verbose = true;
+    } else if (arg == "-n") {
+      if (i + 1 < argc) {
+        numHashes = std::stoi(argv[++i]);
+      } else {
+        fprintf(stderr, "Error: Missing argument for -n\n");
+        usage(argv0, 1);
+        return;
+      }
+    } else if (arg == "--dihedral") {
+      dihedral = true;
+    } else if (arg == "-h" || arg == "--help") {
+      usage(argv0, 0);
+      return;
+    } else if (i == argc - 1) {
+      folderPath = arg;
+    } else {
+      fprintf(stderr, "Unknown argument: %s\n", arg.c_str());
+      usage(argv0, 1);
+      return;
+    }
+  }
+  if (dihedral) { // TODO: remove
+    printf("Dihedral hashes will be computed and printed.\n");
+  }
+
+  // Generate hashes
+  std::vector<facebook::pdq::hashing::Hash256> hashes;
+  float totalReadSeconds = 0, totalHashSeconds = 0;
+  int numErrors = 0, numSuccesses = 0;
+  DIR* dir; // TODO: check this
+  struct dirent* ent;
+  if ((dir = opendir(folderPath.c_str())) != NULL) {
+    while (true) {
+      while ((ent = readdir(dir)) != NULL) {
+        std::string filePath = folderPath + "/" + ent->d_name;
+        if (ent->d_type == DT_REG) {
+          facebook::pdq::hashing::Hash256 hash;
+          int quality;
+          int imageHeightTimesWidth;
+          float readSeconds;
+          float hashSeconds;
+          const char* filename = filePath.c_str();
+          std::cout << "Absolute path: " << std::filesystem::absolute(filename) << std::endl;
+          bool success = facebook::pdq::hashing::pdqHash256FromFile(
+            filename,
+            hash,
+            quality,
+            imageHeightTimesWidth,
+            readSeconds,
+            hashSeconds
+          );
+          if (!success) {
+            numErrors++;
+            fprintf(stderr, "Error reading file: %s\n", filename);
+            continue;
+          }
+          if (verbose) {
+            printf("File: %s\n", filename);
+            printf("Hash: %s\n", hash.format().c_str());
+            printf("Quality: %d\n", quality);
+            printf("Image height * width: %d\n", imageHeightTimesWidth);
+            printf("Read seconds: %.6lf\n", readSeconds);
+            printf("Hash seconds: %.6lf\n", hashSeconds);
+            printf("\n");
+          }
+          hashes.push_back(hash);
+          totalReadSeconds += readSeconds;
+          totalHashSeconds += hashSeconds;
+          numSuccesses++;
+          if (numSuccesses == numHashes) break;
+        }
+      }
+      if (numSuccesses == 0) {
+        fprintf(stderr, "No images found in folder: %s\n", folderPath.c_str());
+        return;
+      }
+      if (numHashes == 0 || numSuccesses == numHashes) break;
+    }
+    closedir(dir);
+  } else {
+    perror("opendir");
+    return;
+  }
+  printf("PHOTO HASHING SECONDS:     %.6lf\n", totalHashSeconds);
+  printf("AVERAGE HASHING SECONDS:   %.6lf\n", totalHashSeconds / (float) numSuccesses);
+  printf("PHOTO READ SECONDS:        %.6lf\n", totalReadSeconds);
+  printf("AVERAGE READ SECONDS:      %.6lf\n", totalReadSeconds / (float) numSuccesses);
+  printf("PHOTO COUNT:               %d\n", (int)hashes.size());
+  printf("ERROR COUNT:               %d\n", numErrors);
+
+}
