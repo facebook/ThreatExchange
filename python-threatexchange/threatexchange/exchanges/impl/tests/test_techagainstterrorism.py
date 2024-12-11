9aa8035b576ae95ec08f9a92e56d7388b6bd5f68
Bruce Pouncey <bruce.pouncey@quantspark.com>
2024-12-11 15:02:33 +0000

    updated tests for deleted

--- python-threatexchange/threatexchange/exchanges/impl/tests/test_techagainstterrorism.py
+++ python-threatexchange/threatexchange/exchanges/impl/tests/test_techagainstterrorism.py
@@ -28,7 +28,7 @@ def test_fetch(exchange: TATSignalExchangeAPI):
     expected_updates = {
         ("video_md5", "123abc"): FetchedSignalMetadata(),
         ("video_md5", "456def"): FetchedSignalMetadata(),
-        ("pdq", "789ghi"): None,
+        None: FetchedSignalMetadata(),
     }
 
     for key in expected_updates:
