099be07135b2bd6dd542152b9eb3bf02395634b0
Brayden Zhong <b8zhong@uwaterloo.ca>
2025-02-01 23:31:27 -0500

    chore: remove unused typeignore

--- hasher-matcher-actioner/src/OpenMediaMatch/tests/test_api.py
+++ hasher-matcher-actioner/src/OpenMediaMatch/tests/test_api.py
@@ -80,7 +80,7 @@ def test_exchange_api_set_auth(app: Flask, client: FlaskClient):
     sample_name = StaticSampleSignalExchangeAPI.get_name()
     tx_name = FBThreatExchangeSignalExchangeAPI.get_name()
     # Monkeypatch installed types
-    storage.exchange_types = {  # type:ignore
+    storage.exchange_types = {
         api_cls.get_name(): api_cls
         for api_cls in (
             FBThreatExchangeSignalExchangeAPI,
