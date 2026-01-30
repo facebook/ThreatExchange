f3645b8a2db953eec3270d267e8a026220ecb0a2
Marco Dimakis <marcodimakis@icloud.com>
2026-01-30 18:54:03 +0200

    Add missing include for std::runtime_error and update TODO comment for frame alignment.

--- vpdq/cpp/vpdq/cpp/hashing/ffmpegutils.cpp
+++ vpdq/cpp/vpdq/cpp/hashing/ffmpegutils.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <stdexcept>
 #include <string>
 
 #include <vpdq/cpp/hashing/ffmpegutils.h>
@@ -55,6 +56,9 @@ AVFramePtr createRGB24Frame(size_t const width, size_t const height) {
   frame->width = width;
   frame->height = height;
 
+  // TODO: Alignment is forced to 1, but the alignment should be variable (0).
+  // This requires updating PDQFrameBufferHasher and VpdqHasher to pass the
+  // linesize as the row stride to PDQ (see GH #1918)
   int ret = av_frame_get_buffer(frame.get(), 1);
   if (ret < 0) {
     char errbuf[AV_ERROR_MAX_STRING_SIZE];
