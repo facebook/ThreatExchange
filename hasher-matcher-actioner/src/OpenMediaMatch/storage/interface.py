7463e85c95deb4ec700b579d043dcb7f0df7b041
Dcallies <dcallies@meta.com>
2026-03-28 10:25:07 -0400

    my

--- hasher-matcher-actioner/src/OpenMediaMatch/storage/interface.py
+++ hasher-matcher-actioner/src/OpenMediaMatch/storage/interface.py
@@ -51,8 +51,10 @@ class IFlaskUnifiedStore(
 
     # TODO: Merge into pytx, remove this version
     @abc.abstractmethod
-    # type: ignore[override]
-    def bank_content_update(self, val: BankContentConfig) -> None:
+    def bank_content_update(
+        self,
+        val: BankContentConfig   # type: ignore[override]
+    ) -> None:
         """Update the content config for a bank"""
 
     # TODO: Merge into pytx, remove this version
