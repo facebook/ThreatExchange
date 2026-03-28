8ce6e2bfd655ce3d8f0e9c28c4099b2ae94ccfa5
David Callies <dcallies@meta.com>
2026-03-28 11:45:32 -0400

    [hma] Add github, aliases to devcontainer (#1966)

--- hasher-matcher-actioner/.devcontainer/postcreate.sh
+++ hasher-matcher-actioner/.devcontainer/postcreate.sh
@@ -2,3 +2,6 @@
 set -e
 
 pip install --no-cache-dir --editable .[all]
+
+cp /workspace/.devcontainer/bash_aliases ~/.bash_aliases
+echo '[ -f ~/.bash_aliases ] && source ~/.bash_aliases' >> ~/.bashrc
